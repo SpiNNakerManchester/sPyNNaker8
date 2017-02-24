c46e68abef9c14c4a9f1b28d206d8335f897b694
Alan <alan.barry.stokes@gmail.com>
2017-02-24 16:59:42 -0500

    input default values added

--- spynnaker8/models/synapse_dynamics/synapse_dynamics_static.py
+++ spynnaker8/models/synapse_dynamics/synapse_dynamics_static.py
@@ -3,9 +3,27 @@ from spynnaker.pyNN.models.neuron.synapse_dynamics.synapse_dynamics_static \
 from pyNN.standardmodels.synapses import StaticSynapse as PyNNStaticSynapse
 
 
-class SynapseDynamicsStatic(CommonSynapseDynamicsStatic, PyNNStaticSynapse):
+class SynapseDynamicsStatic(CommonSynapseDynamicsStatic):
     def __init__(
             self, weight=PyNNStaticSynapse.default_parameters['weight'],
             delay=PyNNStaticSynapse.default_parameters['delay']):
+
         CommonSynapseDynamicsStatic.__init__(self)
-        PyNNStaticSynapse.__init__(self, weight=weight, delay=delay)
+        self._weight = weight
+        self._delay = delay
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, new_value):
+        self._weight = new_value
+
+    @property
+    def delay(self):
+        return self._delay
+
+    @delay.setter
+    def delay(self, new_value):
+        self._delay = new_value
