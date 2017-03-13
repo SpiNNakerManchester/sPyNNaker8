1f80c3b1ada07796a6e624452ae38ae662f9cdd9
alan-stokes <alan.barry.stokes@gmail.com>
2017-03-13 10:31:48 +0000

    added spike source poisson data holder for 0.8

---
+++ spynnaker8/models/model_data_holders/spike_source_poisson_data_holder.py
@@ -0,0 +1,5 @@
+class SpikeSourcePoissonDataHolder(object):
+    def __init__(
+            self, constraints=None,
+            label="SpikeSourcePoisson", rate=1.0, start=0.0, duration=None,
+            seed=None):
