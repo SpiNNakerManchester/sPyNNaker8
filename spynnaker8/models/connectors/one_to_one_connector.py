f377566200981d67cc4b29c9244382a441f8b833
Christian <brenninc@cs.man.ac.uk>
2017-06-06 11:36:10 +0100

    reverted changes correctly VETOED by Dr Stokes

--- spynnaker8/models/connectors/one_to_one_connector.py
+++ spynnaker8/models/connectors/one_to_one_connector.py
@@ -3,8 +3,6 @@ from pyNN.connectors import OneToOneConnector as PyNNOneToOneConnector
 
 from spynnaker.pyNN.models.neural_projections.connectors\
     .one_to_one_connector import OneToOneConnector as CommonOneToOneConnector
-# Exceptions
-from spynnaker8.utilities.exceptions import PyNN7Exception
 
 
 class OneToOneConnector(CommonOneToOneConnector, PyNNOneToOneConnector):
@@ -14,21 +12,7 @@ class OneToOneConnector(CommonOneToOneConnector, PyNNOneToOneConnector):
     pynn_population.py for all i.
     """
 
-    def __init__(self, safe=True, callback=None, weight=None, delay=None):
-        """
-
-        :param safe:
-        :param callback:
-        :param weight: Must be None. Only listed to catch Pynn7 syntax
-        :type weight None
-        :param delay: Must be None. Only listed to catch Pynn7 syntax
-        :type delay: None
-        """
-        if weight is not None:
-            raise PyNN7Exception("weight is now set in the synapse_type")
-        if delay is not None:
-            raise PyNN7Exception("weight is now set in the synapse_type")
-
+    def __init__(self, safe=True, callback=None):
         CommonOneToOneConnector.__init__(
             self, safe=safe, random_number_class=RandomDistribution)
         PyNNOneToOneConnector.__init__(self, safe=safe, callback=callback)
