72024375a6a2d6472cdf27d94fcd6b659067c975
Christian <brenninc@cs.man.ac.uk>
2017-06-06 14:14:47 +0100

    converted to a test

---
+++ p8_integration_tests/test_backwards_compatibility_issues/ssa.py
@@ -0,0 +1,15 @@
+from p8_integration_tests.base_test_case import BaseTestCase
+import spynnaker8 as p
+
+
+class SynfireIfCurrExp(BaseTestCase):
+
+    def test_run(self):
+        p.setup()
+        p.Population(10, p.SpikeSourceArray, {'spike_times': [100, 200]},
+                     label='messed up')
+
+if __name__ == '__main__':
+    p.setup()
+    p.Population(10, p.SpikeSourceArray, {'spike_times': [100, 200]},
+                 label='messed up')
