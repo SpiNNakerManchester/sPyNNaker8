0042ee6bab4ed7fb4d77991cf5ccf45d62b25415
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-06-15 09:42:18 +0100

    Added check for pyNN 8

--- spynnaker8/setup-pynn.py
+++ spynnaker8/setup-pynn.py
@@ -1,6 +1,11 @@
 import pyNN
 import os
 
+if not pyNN.__version__.startswith("0.8"):
+    raise Exception(
+        "PyNN version {} found; SpyNNaker 8 requires PyNN version 0.8".format(
+            pyNN.__version__))
+
 pynn_path = os.path.dirname(pyNN.__file__)
 spinnaker_dir = os.path.join(pynn_path, "spiNNaker")
 spinnaker_init = os.path.join(spinnaker_dir, "__init__.py")
