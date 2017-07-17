94a6e2ba7d5cb9262062c930d5ce88e37388bb00
Christian <brenninc@cs.man.ac.uk>
2017-07-17 13:11:50 +0100

    shortened name in  test

--- unittests/test_integration_using_virtual_baord/test_synfire_200n_10pc_2chips_with_no_delays_spike_recording.py
+++ unittests/test_integration_using_virtual_baord/test_synfire_200n_10pc_2chips_with_no_delays_spike_recording.py
@@ -6,11 +6,11 @@ Synfirechain-like example
 from p8_integration_tests.base_test_case import BaseTestCase
 from p8_integration_tests.scripts.synfire_run import TestRun
 from pacman.model.constraints.placer_constraints\
-    .placer_radial_placement_from_chip_constraint \
-    import PlacerRadialPlacementFromChipConstraint
+    .radial_placement_from_chip_constraint \
+    import RadialPlacementFromChipConstraint
 
 nNeurons = 200  # number of neurons in each population
-constraint = PlacerRadialPlacementFromChipConstraint(3, 3)
+constraint = RadialPlacementFromChipConstraint(3, 3)
 delay = 1
 neurons_per_core = 10
 record_v = False
@@ -23,7 +23,8 @@ class Synfire200n10pc2chipsWithNoDelaysSpikeRecording(BaseTestCase):
     def test_run(self):
         synfire_run.do_run(nNeurons, delay=delay,
                            neurons_per_core=neurons_per_core,
-                           constraint=constraint, record_v=record_v,
+                           constraint=constraint,
+                           record_v=record_v,
                            record_gsyn_exc=record_gsyn,
                            record_gsyn_inh=record_gsyn)
 
