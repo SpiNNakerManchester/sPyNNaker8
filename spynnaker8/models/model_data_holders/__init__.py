de21b5070f131427bde25b2e0b96b48fb2c01ffb
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-07-18 11:00:21 +0100

    Merged in sPyNNaker8ExternalDevicesPlugin

--- spynnaker8/models/model_data_holders/__init__.py
+++ spynnaker8/models/model_data_holders/__init__.py
@@ -5,8 +5,9 @@ from .izk_cond_exp_data_holder import IzkCondExpDataHolder
 from .izk_curr_exp_data_holder import IzkCurrExpDataHolder
 from .spike_source_array_data_holder import SpikeSourceArrayDataHolder
 from .spike_source_poisson_data_holder import SpikeSourcePoissonDataHolder
+from .spike_injector_data_holder import SpikeInjectorDataHolder
 
 __all__ = ["IFCondExpDataHolder", "IFCurrDualExpDataHolder",
            "IFCurrExpDataHolder", "IzkCondExpDataHolder",
            "IzkCurrExpDataHolder", "SpikeSourceArrayDataHolder",
-           "SpikeSourcePoissonDataHolder"]
+           "SpikeSourcePoissonDataHolder", "SpikeInjectorDataHolder"]
