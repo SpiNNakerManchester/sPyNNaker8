5405934472ec8d17886fb5f3eda0f07b25d8e613
alan-stokes <alan.barry.stokes@gmail.com>
2017-08-03 14:01:20 +0100

    fix for #250

--- spynnaker8/models/connectors/__init__.py
+++ spynnaker8/models/connectors/__init__.py
@@ -9,7 +9,7 @@ from .fixed_probability_connector import FixedProbabilityConnector
 from .from_file_connector import FromFileConnector
 from .from_list_connector import FromListConnector
 from .index_based_probability_connector import IndexBasedProbabilityConnector
-from .multapse_connector import MultapseConnector
+from .multapse_connector import MultapseConnector as FixedTotalNumberConnector
 from .one_to_one_connector import OneToOneConnector
 from .small_world_connector import SmallWorldConnector
 
@@ -17,5 +17,5 @@ __all__ = ["AllToAllConnector", "ArrayConnector", "CSAConnector",
            "DistanceDependentProbabilityConnector", "FixedNumberPostConnector",
            "FixedNumberPreConnector", "FixedProbabilityConnector",
            "FromFileConnector", "FromListConnector",
-           "IndexBasedProbabilityConnector", "MultapseConnector",
+           "IndexBasedProbabilityConnector", "FixedTotalNumberConnector",
            "OneToOneConnector", "SmallWorldConnector"]
