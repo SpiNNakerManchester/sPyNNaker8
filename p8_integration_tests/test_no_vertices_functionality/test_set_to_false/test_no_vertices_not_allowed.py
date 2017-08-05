26b5cfdee74e6e677cbb113fd903cb162fe6c295
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-08-05 08:54:42 +0100

    test cleanup

--- p8_integration_tests/test_no_vertices_functionality/test_set_to_false/test_no_vertices_not_allowed.py
+++ p8_integration_tests/test_no_vertices_functionality/test_set_to_false/test_no_vertices_not_allowed.py
@@ -3,19 +3,16 @@ from p8_integration_tests.base_test_case import BaseTestCase
 from spinn_front_end_common.utilities.exceptions import ConfigurationException
 
 
-class TestNoVertices(BaseTestCase):
+class TestNoVerticesNotAllowed(BaseTestCase):
 
-    @staticmethod
-    def test_run():
-        try:
+    def test_exception_raise(self):
+        with self.assertRaises(ConfigurationException):
             p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
             p.run(100)
 
             p.end()
-            raise Exception("failed to raise exception")
-        except ConfigurationException:
-            return
 
 
 if __name__ == '__main__':
-    TestNoVertices.test_run()
+    x = TestNoVerticesNotAllowed("test_exception_raise")
+    x.test_exception_raise()
