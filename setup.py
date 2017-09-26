b87e2cac61ffcdd9685f78077171322d6ff3f568
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2017-09-26 10:59:57 +0100

    unified how package_data is found

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ assert __version__
 
 # Build a list of all project modules, as well as supplementary files
 main_package = "spynnaker8"
-data_extensions = {".aplx", ".boot", ".json", ".xml", ".xsd"}
-config_extensions = {".cfg", ".template"}
+extensions = {".aplx", ".boot", ".cfg", ".json", ".sql", ".template", ".xml",
+              ".xsd"}
 main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
 start = len(main_package_dir)
 packages = []
@@ -20,19 +20,12 @@ for dirname, dirnames, filenames in os.walk(main_package_dir):
         package = "{}{}".format(
             main_package, dirname[start:].replace(os.sep, '.'))
         packages.append(package)
-    ext_set = set()
     for filename in filenames:
         _, ext = os.path.splitext(filename)
-        if ext in data_extensions:
-            ext_set.add(ext)
-        if ext in config_extensions:
+        if ext in extensions:
             package = "{}{}".format(
                 main_package, dirname[start:].replace(os.sep, '.'))
             package_data[package].append(filename)
-    for ext in ext_set:
-        package = "{}{}".format(
-            main_package, dirname[start:].replace(os.sep, '.'))
-        package_data[package].append("*{}".format(ext))
 
 setup(
     name="sPyNNaker8",
