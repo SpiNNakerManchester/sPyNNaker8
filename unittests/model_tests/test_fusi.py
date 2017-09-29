514391770b47a3dcb9aea64ad03b1a43aca4f44b
anna <anna@basl>
2017-09-29 11:46:12 +0100

    update spike timings

--- unittests/model_tests/test_fusi.py
+++ unittests/model_tests/test_fusi.py
@@ -7,15 +7,15 @@ p.setup(1)
 
 simtime = 1000
 
-mysource = [2*range(1, 20)]
-
+# mysource = [10*range(1, 5)]
+mysource = [2,4, 10,14]
 pop_src = p.Population(1, p.SpikeSourceArray, {'spike_times': mysource}, label="src")
-cell_params = {"i_offset":10}
+cell_params = {"i_offset":0, "v_reset":-70}
 pop_ex = p.Population(1, p.IF_curr_exp, cell_params, label="test")
 
 syn_plas = p.STDPMechanism(
      timing_dependence = p.PreOnly(A_plus = 0.5, A_minus = 0.4, th_v_mem=-60),
-        weight_dependence = p.WeightDependenceFusi(w_min=0.0, w_max=5.0), weight=2.5, delay=5.0)
+        weight_dependence = p.WeightDependenceFusi(w_min=1.0, w_max=5.0), weight=2.5, delay=1.0)
 
 proj = p.Projection(
     pop_src, #_plastic,
@@ -33,15 +33,16 @@ v = pop_ex.get_data('v')
 curr = pop_ex.get_data('gsyn_exc')
 spikes = pop_ex.get_data('spikes')
 #pre_spikes = pop_src.get_data('spikes')
-
+print v.segments[0].filter(name='v')[0]
+import numpy
 Figure(
     # raster plot of the presynaptic neuron spike times
-    Panel(SpikeTrain(mysource),
-          yticks=True, markersize=0.5, xlim=(0, 100), xticks=True),
+#     Panel(numpy.array(mysource),
+#           yticks=True, markersize=0.5, xlim=(0, 100), xticks=True),
     Panel(v.segments[0].filter(name='v')[0],
-          yticks=True, markersize=0.5, xlim=(0, 100), xticks=True),
+          yticks=True, markersize=0.5, xlim=(0, 50), xticks=True),
     Panel(spikes.segments[0].spiketrains,
-          yticks=True, markersize=0.5, xlim=(0, 100), xticks=True),
+          yticks=True, markersize=0.5, xlim=(0, 50), xticks=True),
     title="fusi spikes",
     annotations="Simulated with {}".format(p.name()))
 plt.show()
