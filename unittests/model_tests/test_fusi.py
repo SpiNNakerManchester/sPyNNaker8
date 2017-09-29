3273a9d43ae471cc634602bce44170f36b0ca5dd
Anna <anna@basl>
2017-09-29 09:53:36 +0100

    test file plotting

--- unittests/model_tests/test_fusi.py
+++ unittests/model_tests/test_fusi.py
@@ -1,9 +1,15 @@
 import spynnaker8 as p
-
+from pyNN.utility.plotting import Figure, Panel
+import matplotlib.pyplot as plt
+import numpy as np
+from neo.core.spiketrain import SpikeTrain
 p.setup(1)
 
+simtime = 1000
 
-pop_src = p.Population(1, p.SpikeSourceArray, {'spike_times': [[1, 20]]}, label="src")
+mysource = [2*range(1, 20)]
+
+pop_src = p.Population(1, p.SpikeSourceArray, {'spike_times': mysource}, label="src")
 pop_ex = p.Population(1, p.IF_curr_exp, {}, label="test")
 
 
@@ -19,12 +25,27 @@ proj = p.Projection(
     )
 
 pop_ex.record(['v', 'gsyn_exc', 'gsyn_inh', 'spikes'])
+#pop_src.record(['v', 'gsyn_exc', 'gsyn_inh', 'spikes'])
 
-p.run(100)
+p.run(simtime)
 
 v = pop_ex.get_data('v')
 curr = pop_ex.get_data('gsyn_exc')
 spikes = pop_ex.get_data('spikes')
+#pre_spikes = pop_src.get_data('spikes')
+
+Figure(
+    # raster plot of the presynaptic neuron spike times
+    Panel(SpikeTrain(mysource),
+          yticks=True, markersize=0.5, xlim=(0, 100), xticks=True),
+    Panel(v.segments[0].filter(name='v')[0],
+          yticks=True, markersize=0.5, xlim=(0, 100), xticks=True),
+    Panel(spikes.segments[0].spiketrains,
+          yticks=True, markersize=0.5, xlim=(0, 100), xticks=True),
+    title="fusi spikes",
+    annotations="Simulated with {}".format(p.name()))
+plt.show()
+
 
 p.end()
 print "\n job done"
