bd879a5d65e8dd3482071cc22c5caa73e39fb228
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2017-11-02 14:35:08 +0000

    extra randmon range test

--- unittests/utilities_tests/test_random_ranged.py
+++ unittests/utilities_tests/test_random_ranged.py
@@ -8,3 +8,11 @@ def test_uniform():
     rd["a"] = RandomDistribution("uniform", parameters_pos=[-65.0, -55.0])
     ranges = rd["a"].get_ranges()
     assert 10 == len(ranges)
+
+
+def test_check():
+    import inspect
+    a = RandomDistribution("uniform", parameters_pos=[-65.0, -55.0])
+    arg_spec = inspect.getargspec(a.next)
+    assert "n" in arg_spec.args
+
