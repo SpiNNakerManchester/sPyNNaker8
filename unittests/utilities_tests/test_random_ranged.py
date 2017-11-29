89393e08679012ad3658311ff05619de37ecc707
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2017-11-29 11:31:25 +0000

    flake8

--- unittests/utilities_tests/test_random_ranged.py
+++ unittests/utilities_tests/test_random_ranged.py
@@ -7,7 +7,6 @@ from p8_integration_tests.base_test_case import BaseTestCase
 
 class TestRanged(BaseTestCase):
 
-
     def test_uniform(self):
         # Need to do setup to get a pynn version
         p.setup(10)
