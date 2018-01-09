bdce3a9c4dc80a9bd8eaabecc39793e91dea4ed4
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-01-09 16:40:45 +0000

    Started looking at Multapse / FixedTotalNumber connector in pynn0.8

--- spynnaker8/models/connectors/multapse_connector.py
+++ spynnaker8/models/connectors/multapse_connector.py
@@ -19,3 +19,11 @@ class MultapseConnector(CommonMultapseConnector):
         CommonMultapseConnector.__init__(
             self, num_synapses=num_synapses, safe=safe, verbose=verbose)
         self.set_weights_and_delays(weights, delays)
+
+    def get_rng_next(self, num_synapses, prob_connect):
+        # This needs to be edited to work with PyNN 0.8+
+        # equivalent PyNN 0.7 call with the allowed "multinomial"
+        # returns an array, this with "binomial" only returns a single value...
+        return self._rng.next(1, distribution="binomial",
+                              parameters = {'n': num_synapses,
+                                            'p': prob_connect[0]})
