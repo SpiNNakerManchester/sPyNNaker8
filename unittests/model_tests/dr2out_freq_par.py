8f69eca12e28175f41a7b55d0005de8e7e15f07b
366anna <366anna@gmail.com>
2018-02-06 18:07:15 +0100

    Updated drive rate to output rate script
    Simplified the script by removing the input neuron, leaving 
    only the driving neuron with static synapse.

--- unittests/model_tests/dr2out_freq_par.py
+++ unittests/model_tests/dr2out_freq_par.py
@@ -1,3 +1,5 @@
+# Calculate average output rate for a range of drive rates
+# the network consists of one Fusi neuron, and one driving Poisson source, connected with a static connection, repeated 6000 times
 import spynnaker8 as p
 from pyNN.utility.plotting import Figure, Panel, DataTable
 import matplotlib.pyplot as plt
@@ -16,14 +18,6 @@ from neo.io import PyNNTextIO
 from example_graph_params import *
 import time
 
-def gen_poisson_spikes(rate, simtime):
-    spiketimes = []
-    prob = 0.001*rate
-    for i in xrange(simtime):
-        if random.random() < prob:
-            spiketimes.append(i)
-    return spiketimes
-
 def print_spikes(trains):
     nseg = len(trains.segments)
     for i in range(nseg):
@@ -41,54 +35,25 @@ p.setup(1)
 
 simtime = 300
 n_runs = 3
-w0 = 0.0
-pre_rate = 50
-drive_rates = np.arange(100, 150, 10) # driving source rates
+drive_rates = np.arange(10, 400, 10) # driving source rates
 n_rates = drive_rates.size
-max_out_rate = 200
 output_file = "data/dr2out_"+timestr
 output_ext = ".txt"
 
-avg_out_rate = np.zeros(n_rates) # number of weight transitions for each spiking rate of the output neuron for 0 to 200
+avg_out_rate = np.zeros(n_rates) # calculated average output rate for each drive rate
 
-n_nrn = 1000  # total number of neurons in each run
+n_nrn = 6000  # total number of neurons in each run
 
-p.set_number_of_neurons_per_core(p.extra_models.IFCurrExpCa2Concentration, 100)
-p.set_number_of_neurons_per_core(p.SpikeSourcePoisson, 100)
+p.set_number_of_neurons_per_core(p.extra_models.IFCurrExpCa2Concentration, 200)
 
 
-pop_src = p.Population(n_nrn, p.SpikeSourcePoisson(rate=pre_rate), label="src")
 pop_src2 = p.Population(n_nrn, p.SpikeSourcePoisson(rate=100), label="drive")
 cell_params = {"i_offset":0.0,  "tau_ca2":150, "i_alpha":1., "i_ca2":3.,  'v_reset':-65}
 pop_ex = p.Population(n_nrn, p.extra_models.IFCurrExpCa2Concentration, cell_params, label="test")
-#pop_ex = p.Population(n_nrn, p.IF_curr_exp(), label="test")
-
-
-syn_plas = p.STDPMechanism(
-     timing_dependence = p.PreOnly(A_plus = 0.15*w_max*w_mult, A_minus = 0.15*w_max*w_mult, th_v_mem=V_th, th_ca_up_l = Ca_th_l, th_ca_up_h = Ca_th_h2, th_ca_dn_l = Ca_th_l, th_ca_dn_h = Ca_th_h1),
-        weight_dependence = p.WeightDependenceFusi(w_min=w_min*w_mult, w_max=w_max*w_mult, w_drift=w_drift*w_mult, th_w=th_w * w_mult), weight=w0*w_mult, delay=1.0)
-
-
-proj = p.Projection(
-    pop_src,
-    pop_ex,
-    p.OneToOneConnector(),
-    synapse_type=syn_plas, receptor_type='excitatory'
-    )
 
 proj2 = p.Projection(pop_src2,  pop_ex,  p.OneToOneConnector(),
-               synapse_type=p.StaticSynapse(weight=2.0),  receptor_type='excitatory')
+               synapse_type=p.StaticSynapse(weight=3.0),  receptor_type='excitatory')
 
-
-# proj2 = p.Projection(
-#     pop_src2,
-#     pop_ex,
-#     p.OneToOneConnector(),
-#     syn,
-#     receptor_type='excitatory'
-#     )
-
-pop_src.record('spikes')
 pop_src2.record(['spikes'])
 pop_ex.record('spikes')
 nseg = 0
@@ -102,33 +67,29 @@ for ri in range(n_rates):
         p.run(simtime)
         new_rates = np.zeros(n_nrn, dtype=int)
 
-        #print_spikes(pop_src2.get_data('spikes'))
-#        trains = pop_ex.get_data('spikes').segments[nseg].spiketrains
+        trains2 = pop_src2.get_data('spikes')
+        trains2 = trains2.segments[nseg].spiketrains
+
+
         trains = pop_ex.get_data('spikes').segments[nseg].spiketrains
-#         trains2 = pop_src2.get_data('spikes')
-#         trains2 = trains2.segments[nseg].spiketrains
 
         for n in range(n_nrn):
             n_spikes = trains[n].shape[0]
             #print dr_r, n,":", trains[n]
             print n_spikes
-            #n_spikes2 = trains2[n].shape[0]
-            #print n_spikes2
+            n_spikes2 = trains2[n].shape[0]
+            print n_spikes2
             avg_out_rate[ri] = avg_out_rate[ri] + n_spikes
 
 
 
         print nseg, dr_r
         nseg = nseg+1
-        #new_w = proj.get('weight', format='list', with_address=False)
 
 
         p.reset()
-#    probs.tofile(output_file+"_"+str(r)+output_ext, sep='\t', format='%10.5f')
 
 
-
-#p.run(50)
 avg_out_rate = avg_out_rate/n_nrn/n_runs*1000.0/simtime
 
 print avg_out_rate
