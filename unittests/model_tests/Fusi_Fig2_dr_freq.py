f04dfe34497c4bd7c18b4811146253e3691d23c9
366anna <366anna@gmail.com>
2018-02-08 18:16:16 +0100

    Fig 2 for large board
    Has 400 populations, for different input and drive rates

--- unittests/model_tests/Fusi_Fig2_dr_freq.py
+++ unittests/model_tests/Fusi_Fig2_dr_freq.py
@@ -14,22 +14,42 @@ import matplotlib.pyplot as plt
 from neo.io import PyNNNumpyIO
 from neo.io import AsciiSpikeTrainIO
 from neo.io import PyNNTextIO
-from example_graph_params import *
+#from example_graph_params import *
 import time
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
+do_LTP = True # true for LTPs, false for LTDs
+#do_LTP = False # true for LTPs, false for LTDs
+do_save=False # save the data or not
 
 p.setup(1)
+p.set_number_of_neurons_per_core(p.extra_models.IFCurrExpCa2Concentration, 200)
+
+w_min = 0.0
+w_max = 1.0
+w_drift = .0035
+th_w = 0.50
+w_mult = 2.0
+V_th = -54.0
+Ca_th_l = 3.0
+Ca_th_h1 = 4.0
+Ca_th_h2 = 13.0
 
 simtime = 300
-n_runs = 30
-w0 = 0.0 #initial weight for LTP
-#w0 = 1.0 #initial weight for LTD
-pre_rates = np.arange(40, 55, 10) # pre-synaptic neuron rates
+n_runs = 300
+n_nrn = 200 # total number of neurons in each population
+
+if do_LTP:
+    w0 = 0.0 #initial weight for LTP
+    drive_rates = np.arange(0, 400, 10) # driving source rates for LTPS
+else:
+    w0 = 1.0 #initial weight for LTD
+    drive_rates = np.arange(0, 200, 5) # driving source rates for LTDS
+
+w_mult  = 2.0
+pre_rates = np.arange(5, 55, 5) # pre-synaptic neuron rates
 n_pre_rates = pre_rates.shape[0]
-drive_rates = np.arange(0, 400, 40) # driving source rates for LTPS
-#drive_rates = np.arange(0, 300, 30) # driving source rates for LTDS
 n_drive_rates = drive_rates.shape[0]
 n_rates = drive_rates.size
 max_out_rate = 200
@@ -40,6 +60,7 @@ n_trans = np.zeros((n_pre_rates, n_drive_rates)) # number of weight transitions 
 n_tot = np.zeros((n_pre_rates, n_drive_rates)) # number of sims ran for each spiking rate of the output neuron (needed to calculate transition probability)
 
 drx = np.arange(0, 400, 10)
+# output frequencies for driving frequencies from 0 to 400, with step 10 and no input
 dr2outfr = [   0, 0.16648148,    1.1612963,     3.11944444,    6.05296296,    9.62148148,
    13.60481481,   18.06703704,   22.73611111,   27.66277778,   32.37333333,
    37.15925926,   42.28777778,   47.39092593,   52.21037037,   57.26425926,
@@ -49,12 +70,21 @@ dr2outfr = [   0, 0.16648148,    1.1612963,     3.11944444,    6.05296296,    9.
   128.63907407,  132.65185185,  136.53388889,  140.15388889,  143.59981481,
   146.63222222,  149.14685185,  151.49296296,  153.66962963]
 
+# for w=3.0
+# dr2outfr = [0,    1.395,         4.96648148,    9.83666667,   15.55796296 ,  21.92481481,
+#    28.54833333,   35.24277778,   42.14074074,   49.2487037,    56.06703704,
+#    62.83759259,   69.6362963,    76.59666667,   82.91351852,   89.53481481,
+#    95.93555556,  102.27685185,  108.46407407,  114.67666667,  120.56277778,
+#   126.42537037,  132.31259259,  138.00407407,  143.53833333,  149.05981481,
+#   154.3962963,   159.76666667,  164.79759259,  170.1087037,   174.76259259,
+#   179.71462963,  184.59574074,  188.99555556,  193.78555556,  197.70277778,
+#   201.16777778,  204.29333333,  206.75833333,  209.09314815]
+
 f_dr2out = interp1d(drx, dr2outfr)
 
 
-n_nrn = 200 # total number of neurons in each population
 
-cell_params = {"i_offset":0.0,  "tau_ca2":175, "i_alpha":1., "i_ca2":3.,  'v_reset':-65}
+cell_params = {"i_offset":0.0,  "tau_ca2":150, "i_alpha":1., "i_ca2":3.,  'v_reset':-65}
 
 pops = []
 pops_src = []
@@ -79,7 +109,7 @@ for pre_r in pre_rates:
             )
 
         proj2 = p.Projection(pop_src2,  pop_ex,  p.OneToOneConnector(),
-               synapse_type=p.StaticSynapse(weight=2.0),  receptor_type='excitatory')
+               synapse_type=p.StaticSynapse(weight=w_mult),  receptor_type='excitatory')
 
 
         pop_ex.record(['spikes'])
@@ -134,8 +164,8 @@ for r in range(n_runs):
 probs = n_trans / n_tot
 
 print probs
-
-probs.tofile(output_file+"_full"+output_ext, sep='\t', format='%10.5f')
+if do_save:
+    probs.tofile(output_file+"_full"+output_ext, sep='\t', format='%10.5f')
 
 xs = f_dr2out(drive_rates)
 
