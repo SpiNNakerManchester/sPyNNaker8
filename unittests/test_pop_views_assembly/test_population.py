643ab4efc2e3792187ef29c6bbc4aabcf7da0b58
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-02-09 09:20:17 +0000

    flake8

--- unittests/test_pop_views_assembly/test_population.py
+++ unittests/test_pop_views_assembly/test_population.py
@@ -13,7 +13,8 @@ class Test_Population(BaseTestCase):
             pop_1 = sim.Population(n_neurons, sim.IF_curr_exp(), label=label)
             self.assertEquals(n_neurons, pop_1.size)
             self.assertEquals(label, pop_1.label)
-            self.assertEquals(sim.IF_curr_exp.build_model(), type(pop_1.celltype))
+            self.assertEquals(sim.IF_curr_exp.build_model(),
+                              type(pop_1.celltype))
             v_init = -60
             pop_1.initialize(v=v_init)
             initial_values = pop_1.initial_values
@@ -49,4 +50,27 @@ class Test_Population(BaseTestCase):
                 msg = "Depends on https://github.com/SpiNNakerManchester" \
                       "/sPyNNaker8/pull/73"
                 raise SkipTest(msg)
+            sim.end()
+
+        def test_set(self):
+            n_neurons = 5
+            label = "pop_1"
+            sim.setup(timestep=1.0)
+            pop_1 = sim.Population(n_neurons, sim.IF_curr_exp(), label=label)
+            pop_1.set(v=2)
+            sim.end()
+
+        def test_selector(self):
+            n_neurons = 5
+            label = "pop_1"
+            sim.setup(timestep=1.0)
+            pop_1 = sim.Population(n_neurons, sim.IF_curr_exp(), label=label)
+            pop_1.set(tau_m=2)
+            values = pop_1.get("tau_m")
+            self.assertEqual([2, 2, 2, 2, 2], values)
+            values = pop_1.get_by_selector(slice(1, 3), "tau_m")
+            self.assertEquals([2, 2], values)
+            pop_1.set_by_selector(slice(1, 3), "tau_m", 3)
+            values = pop_1.get("tau_m")
+            self.assertEqual([2, 3, 3, 2, 2], values)
             sim.end()
