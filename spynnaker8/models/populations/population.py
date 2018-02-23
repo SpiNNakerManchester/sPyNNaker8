a8a2e9733b23ef350556f335a95909e8f6aae651
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2018-02-23 16:47:30 +0100

    views in progress

--- spynnaker8/models/populations/population.py
+++ spynnaker8/models/populations/population.py
@@ -310,6 +310,13 @@ class Population(PyNNPopulationCommon, Recorder):
             raise Exception("Population does not support changes after run")
         self._vertex.set_initial_value(variable, value, selector)
 
+    # NONE PYNN API CALL
+    def get_initial_values(self, selector=None):
+        """ See AbstractPopulationInitializable.get_initial_values"""
+        if not self._vertex_population_initializable:
+            raise KeyError("Population does not support the initialisation")
+        return self._vertex.get_initial_values(selector)
+
     def get(self, parameter_names, gather=False, simplify=True):
         if simplify is not True:
             logger.warning("The simplify value is ignored if not set to true")
--- spynnaker8/models/populations/population_view.py
+++ spynnaker8/models/populations/population_view.py
@@ -1,45 +1,398 @@
-import numpy
+import logging
+from pyNN import descriptions
 
-from pyNN import common as pynn_common
-from pyNN.parameters import simplify, ParameterSpace
-from spinn_front_end_common.utilities import globals_variables
-from .assembly import Assembly
+from spinn_utilities.ranged.abstract_sized import AbstractSized
+from spynnaker8.models.populations import IDMixin
+
+logger = logging.getLogger(__name__)
 
 
-class PopulationView(pynn_common.PopulationView):
-    _assembly_class = Assembly
+class PopulationView(object):
+
+    def __init__(self, parent, selector, label=None):
+        """
+        A view of a subset of neurons within a Population.
+
+        In most ways, Populations and PopulationViews have the same behaviour,
+            i.e.they can be recorded, connected with Projections, etc.
+        It should be noted that any changes to neurons in a PopulationView
+            will be reflected in the parent Population and vice versa.
+
+        It is possible to have views of views.
+
+        param selector: a slice or numpy mask array.
+            The mask array should either be a boolean array (ideally) of the
+            same size as the parent,
+            or an integer array containing cell indices,
+            i.e. if  p.size == 5:
+                PopulationView(p, array([False, False, True, False, True]))
+                PopulationView(p, array([2, 4]))
+                PopulationView(p, slice(2, 5, 2))
+           will all create the same view.
+
+        Note: Selector to Id is actually handled by AbstractSized.
+        """
+        self._parent = parent
+        sized = AbstractSized(parent.size)
+        ids = sized.selector_to_ids(selector, warn=True)
+
+        if isinstance(parent, PopulationView):
+            self._population =  parent.grandparent
+            self._indexes = parent.index_in_grandparent(ids)
+        else:
+            self._population = parent
+            self._indexes = ids
+        self._mask = selector
+        self._label = label
 
     @property
-    def _simulator(self):
-        return globals_variables.get_simulator()
+    def size(self):
+        """The total number of neurons in the Population."""
+        return len(self._indexes)
 
-    # --------------------------------------------------------------------------
-    # Internal PyNN methods
-    # --------------------------------------------------------------------------
-    def _get_parameters(self, *names):
+    @property
+    def label(self):
+        """A label for the Population."""
+        return self.label
+
+    @property
+    def celltype(self):
+        """ The type of neurons making up the Population."""
+        return self._population.celltype
+
+    @property
+    def initial_values(self):
+        """ A dict containing the initial values of the state variables"""
+        return self._population.get_initial_values(self, self._indexes)
+
+    @property
+    def parent(self):
+        """ A reference to the parent Population(that this is a view of)."""
+        return self._parent
+
+    @property
+    def mask(self):
+        """  The selector mask that was used to create this view."""
+        return self._mask
+
+    @property
+    def all_cells(self):
+        """  An array containing the cell ids of all neurons in the
+            Population(all MPI nodes). """
+        cells = []
+        for id in self._indexes:
+            cells.append(IDMixin(self, id))
+        return cells
+
+    @property
+    def local_cells(self):
         """
-        return a ParameterSpace containing native parameters
+        An array containing the cell ids of those neurons in the Population
+            that exist on the local MPI node.
         """
-        parameter_dict = {}
-        for name in names:
-            value = self.parent._parameters[name]
-            if isinstance(value, numpy.ndarray):
-                value = value[self.mask]
-            parameter_dict[name] = simplify(value)
-        return ParameterSpace(parameter_dict, shape=(self.size,))
+        logger.warning("local calls do not really make sense on sPyNNaker so "
+                       "local_cells just returns all_cells")
+        return self.all_cells
 
-    def _set_parameters(self, parameter_space):
-        # Loop through parameters we're setting, evaluate the value we're
-        # Setting and assign it to the masked section of
-        # parent's parameters this view represents
-        for name, value in parameter_space.items():
-            evaluated_value = value.evaluate(simplify=True)
-            self.parent._parameters[name][self.mask] = evaluated_value
+    def __add__(other):
+        """ A Population / PopulationView can be added to another
+            Population, PopulationView or Assembly, returning an Assembly.
+        """
+        raise NotImplementedError
 
-    def _set_initial_value_array(self, variable, initial_values):
-        # Initial values are handled by common.Population
-        # so we can evaluate them at build-time
-        pass
+    def __getitem__(self, index):
+        """
+        Return either a single cell(ID object) from the Population,
+            if index is an integer, or a subset of the cells
+            (PopulationView object), if index is a slice or array.
 
-    def _get_view(self, selector, label=None):
-        return PopulationView(self, selector, label)
+        Note that __getitem__ is called when using[] access, e.g. p =
+            Population(...) p[2] is equivalent to p.__getitem__(2).p[3:6] is
+            equivalent to p.__getitem__(slice(3, 6))
+        """
+        if isinstance(index, [int, long]):
+            return IDMixin(self, index)
+        else:
+            return PopulationView(self, index, label=self.label+"_" + str(
+                index))
+
+    def __iter__(self):
+        """ Iterator over cell ids on the local node."""
+        for id in self._indexes:
+            yield IDMixin(self, id)
+
+    def __len__(self):
+        """  Return the total number of cells in the population(all nodes)."""
+        return len(self._indexes)
+
+    def all(self):
+        """ Iterator over cell ids on all MPI nodes."""
+        for id in self._indexes:
+            yield IDMixin(self, id)
+
+    def can_record(self, variable):
+        """ Determine whether variable can be recorded from this population."""
+        return self._population.can_record(variable)
+
+    @property
+    def conductance_based(self):
+        """
+        Indicates whether the post - synaptic response is modelled as a
+        change in conductance or a change in current.
+        """
+        return self._population.conductance_base
+
+    def describe(self, template='populationview_default.txt',
+                 engine='default'):
+        """ Returns a human - readable description of the population view.
+
+        The output may be customized by specifying a different template
+            togther with an associated template engine (see pyNN.descriptions).
+
+        If template is None, then a dictionary containing the template
+            context will be returned.
+        """
+        context = {"label": self.label,
+                   "parent": self.parent.label,
+                   "mask": self.mask,
+                   "size": self.size}
+        context.update(self.annotations)
+        return descriptions.render(engine, template, context)
+
+    def find_units(self, variable):
+        """ NO PyNN description of this method """
+        return self._population.find_units(variable)
+
+    def get(self, parameter_names, gather=False, simplify=True):
+        """
+        Get the values of the given parameters for every local cell in
+            the population, or, if gather=True,
+            for all cells in the population.
+
+        Values will be expressed in the standard PyNN units(i.e.millivolts,
+        nanoamps, milliseconds, microsiemens, nanofarads, event per second).
+        """
+        if simplify is not True:
+            logger.warning("The simplify value is ignored if not set to true")
+
+        return self._population.get_by_selector(self._indexes, parameter_name)
+
+    def getSpikes(self, *args, **kwargs):
+        """ Deprecated.Use get_data('spikes') instead. """
+
+    def get_data(self, variables='all', gather=True, clear=False):
+        """
+        Return a Neo Block containing the data(spikes, state variables)
+            recorded from the Population.
+
+        variables - either a single variable name or a list of variable names
+
+        Variables must have been previously recorded,
+            otherwise an Exception will be raised.
+
+        For parallel simulators, if gather is True, all data will be
+            gathered to all nodes and the Neo Block will contain data
+            from all nodes.
+        Otherwise, the Neo Block will contain only data from the cells
+            simulated on the local node.
+
+        If clear is True, recorded data will be deleted from the Population.
+        """
+
+    def get_gsyn(self, *args, **kwargs):
+        """ Deprecated.Use get_data(['gsyn_exc', 'gsyn_inh']) instead."""
+
+    def get_spike_counts(self, gather=True):
+        """ Returns a dict containing the number of spikes for each neuron.
+
+        The dict keys are neuron IDs, not indices.
+        """
+
+    def get_v(self, *args, **kwargs):
+        """ Deprecated.Use get_data('v') instead. """
+
+    @property
+    def grandparent(self):
+        """
+        Returns the parent Population at the root of the tree(since the
+            immediate parent may itself be a PopulationView).
+
+        The name "grandparent" is of course a little misleading, as it could
+            be just the parent, or the great, great, great, ..., grandparent.
+        """
+
+    def id_to_index(self, id):
+        """
+        Given the ID(s) of cell(s) in the PopulationView, return its /
+            their index / indices(order in the PopulationView).
+
+        assert pv.id_to_index(pv[3]) == 3
+        """
+
+    def index_in_grandparent(self, indices):
+        """
+        Given an array of indices, return the indices in the parent
+            population at the root of the tree.
+        """
+
+    @property
+    def initial_values(self):
+        """ NO PyNN description of this method """
+
+
+    def initialize(self, **initial_values):
+        """
+        Set initial values of state variables, e.g.the membrane potential.
+
+        Values passed to initialize() may be:
+            single numeric values(all neurons set to the same value)
+        RandomDistribution objects
+        lists / arrays of numbers of the same size as the population
+        mapping functions, where a mapping function accepts a single
+            argument(the cell index) and returns a single number.
+
+        Values should be expressed in the standard PyNN units(
+           i.e.millivolts, nanoamps, milliseconds, microsiemens, nanofarads,
+           event per second).
+
+        Examples:
+            p.initialize(v=-70.0)
+            p.initialize(v=rand_distr, gsyn_exc=0.0)
+            p.initialize(v=lambda i: -65 + i / 10.0)
+        """
+
+    def inject(self, current_source):
+        """ Connect a current source to all cells in the Population."""
+
+    def is_local(self, id):
+        """
+        Indicates whether the cell with the given ID exists on the
+            local MPI node.
+        """
+
+    @property
+    def local_size(self):
+        """
+        Return the number of cells in the population on the local MPI node
+        """
+
+    def meanSpikeCount(self, *args, **kwargs):
+        """ Deprecated. Use mean_spike_count() instead. """
+
+    def mean_spike_count(self, gather=True):
+        """ Returns the mean number of spikes per neuron. """
+
+    def nearest(self, position):
+        """ Return the neuron closest to the specified position."""
+
+    @property
+    def position_generator(self):
+        """ NO PyNN description of this method """
+
+    @property
+    def positions(self):
+        """ NO PyNN description of this method """
+
+    def printSpikes(self, *args, **kwargs):
+        """ Deprecated. Use write_data(file, 'spikes') instead. """
+
+    def print_gsyn(self, *args, **kwargs):
+        """
+        Deprecated. Use write_data(file, ['gsyn_exc', 'gsyn_inh']) instead.
+        """
+
+    def print_v(self, *args, **kwargs):
+        """  Deprecated. Use write_data(file, 'v') instead."""
+
+    def receptor_types(self):
+        """ NO PyNN description of this method """
+
+    def record(self, variables, to_file=None, sampling_interval=None):
+        """
+        Record the specified variable or variables for all cells in the
+            Population or view.
+
+        variables may be either a single variable name or a list of variable
+            names.
+        For a given celltype class , celltype.recordable contains a list of
+            variables that can be recorded for that celltype.
+
+        If specified, to_file should be a Neo IO instance and write_data()
+            will be automatically called when end() is called.
+
+        sampling_interval should be a value in milliseconds, and an integer
+            multiple of the simulation timestep.
+        """
+
+    def record_gsyn(self, *args, **kwargs):
+        """ Deprecated. Use record(['gsyn_exc', 'gsyn_inh']) instead. """
+
+    def record_v(self, *args, **kwargs):
+        """ Deprecated. Use record('v') instead. """
+
+    def rset(self, *args, **kwargs):
+        """ Deprecated. Use set(parametername=rand_distr) instead. """
+
+    def sample(self, n, rng=None):
+        """
+        Randomly sample n cells from the Population, and return a
+            PopulationView object.
+        """
+
+    def save_positions(self, file):
+        """ Save positions to file. The output format is index x y z """
+
+    def set(self, **parameters):
+        """
+        Set one or more parameters for every cell in the population.
+
+        Values passed to set() may be:
+            single values
+            RandomDistribution objects
+            lists / arrays of values of the same size as the population
+            mapping functions, where a mapping function accepts a single
+                argument(the cell index) and returns a single value.
+
+        Here, a "single value" may be either a single number or a list /
+            array of numbers(e.g. for spike times).
+
+        Values should be expressed in the standard PyNN units
+            (i.e.millivolts, nanoamps, milliseconds, microsiemens, nanofarads,
+            event per second).
+
+        Examples:
+            p.set(tau_m=20.0, v_rest=-65).
+            p.set(spike_times=[0.3, 0.7, 0.9, 1.4])
+            p.set(cm=rand_distr, tau_m=lambda i: 10 + i / 10.0)
+        """
+
+    @property
+    def structure(self):
+        """ The spatial structure of the parent Population. """
+
+    def tset(self, *args, **kwargs):
+        """ Deprecated. Use set(parametername=value_array) instead. """
+
+    def write_data(self, io, variables='all', gather=True, clear=False,
+                   annotations=None):
+        """
+        Write recorded data to file, using one of the file formats
+            supported by Neo.
+
+        io: a Neo IO instance variables: either a single variable name or a
+            list of variable names.
+
+        Variables must have been previously recorded, otherwise an Exception
+            will be raised.
+
+        For parallel simulators, if gather is True, all data will be
+            gathered to the master node and a single output file created there.
+            Otherwise, a file will be written on each node,
+            containing only data from the cells simulated on that node.
+
+        If clear is True, recorded data will be deleted from the Population.
+
+        annotations should be a dict containing simple data types such as
+            numbers and strings. The contents will be written into the output
+            data file as metadata.
+        """
