fc2ea9d5e1b210f0a35968fa0137a2b420a3da02
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-02-27 16:42:43 +0000

    Start work on index-based probability connector

--- spynnaker8/models/connectors/index_based_probability_connector.py
+++ spynnaker8/models/connectors/index_based_probability_connector.py
@@ -1,18 +1,17 @@
-from spynnaker.pyNN.models.neural_projections.connectors \
-    import AbstractConnector
-from pyNN.connectors import IndexBasedProbabilityConnector as \
-    PyNNIndexBasedProbabilityConnector
+from spynnaker.pyNN.models.neural_projections.connectors import \
+    IndexBasedProbabilityConnector as CommonIndexBasedProbabilityConnector
+#from pyNN.connectors import IndexBasedProbabilityConnector as \
+#    PyNNIndexBasedProbabilityConnector
 
 
-class IndexBasedProbabilityConnector(
-        AbstractConnector, PyNNIndexBasedProbabilityConnector):
+class IndexBasedProbabilityConnector(CommonIndexBasedProbabilityConnector):
 
     def __init__(
             self, index_expression, allow_self_connections=True, rng=None,
-            safe=True, callback=None):
-        AbstractConnector.__init__(self, safe=safe)
-        PyNNIndexBasedProbabilityConnector.__init__(
+            safe=True, callback=None, verbose=False):
+
+        CommonIndexBasedProbabilityConnector.__init__(
             self, index_expression=index_expression,
             allow_self_connections=allow_self_connections, rng=rng,
-            safe=safe, callback=callback)
-        raise NotImplementedError
+            safe=safe, callback=callback, verbose=verbose)
+
