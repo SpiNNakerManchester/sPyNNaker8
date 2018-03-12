55b9ac5eb19cbe6b1e33d6fc7e3ce18d09b9d901
ASUS <ASUS@Lultra.manchester.ac.uk>
2018-03-12 15:56:45 +0000

    update fig__5_18

--- p8_integration_tests/test_stp/fig__5_18.py
+++ p8_integration_tests/test_stp/fig__5_18.py
@@ -5,68 +5,95 @@ from pyNN.utility.plotting import Figure, Panel
 import matplotlib.pyplot as plt
 
 
+def processing_data(data, frequencies):
+    data = [line for line in data if not line.startswith('[INFO]') and not line == '\n']
+    #for line in data:
+        #print(line)
+    times = []
+    current_stp_traces = []
+    for line in data:
+        value = line.split(': ', 1)[-1]
+        if line.startswith(' time'):
+            times.append(value)
+        elif line.startswith(' current'):
+            current_stp_traces.append(value)
+        else:
+            print "Line without meaningful value"
+    # converting strings to numbers
+    times = map(int, times)
+    current_stp_traces = map(float, current_stp_traces)
+    # subtract the initial_run value from every time
+    times = map(lambda x: x - 100, times)
+    # compute average release probabilities for every simulated frequency
+    rt_scaling = 10
+    runtime = len(spiking_frequencies)*200*rt_scaling
+    start_time = 0
+    freq_stp_traces = []
+    norm_stp_traces = []
+    for freq in spiking_frequencies:
+        stps_traces = []
+        for i in range(len(times)):
+            if times[i] >= start_time and times[i] <= start_time+200*rt_scaling:
+                stps_traces.append(current_stp_traces[i])
+        freq_stp_traces.append(np.mean(stps_traces))
+        # normalization: multiplying probabilities with firing rates
+        norm_stp_traces.append(np.mean(stps_traces)*freq)
+        start_time = start_time + 200*rt_scaling
+    return freq_stp_traces, norm_stp_traces
+
+
+
 output_path = 'C:\\Users\\Lultra\\git\\'
 filename1 = 'out_fac.txt'
 filename2 = 'out_dep.txt'
 
-with open(output_path + filename) as f:
-    data = f.readlines()
+with open(output_path + filename1) as f1:
+    data_fac = f1.readlines()
+with open(output_path + filename2) as f2:
+    data_dep = f2.readlines()
 
-data = [line for line in data if not line.startswith('[INFO]') and not line == '\n']
+spiking_frequencies = np.linspace(0, 100, 21)
+freq_stp_traces_fac, norm_stp_traces_fac = processing_data(data_fac, spiking_frequencies)
+freq_stp_traces_dep, norm_stp_traces_dep = processing_data(data_dep, spiking_frequencies)
 
-#for line in data:
-    #print(line)
-
-times = []
-current_stp_traces = []
-
-for line in data:
-    value = line.split(': ', 1)[-1]
-    if line.startswith(' time'):
-        times.append(value)
-    elif line.startswith(' current'):
-        current_stp_traces.append(value)
-    else:
-        print "Line without meaningful value"
-
-# converting strings to numbers
-times = map(int, times)
-current_stp_traces = map(float, current_stp_traces)
-
-# subtract the initial_run value from every time
-times = map(lambda x: x - 100, times)
-
-
-rt_scaling = 10
-
-# normalization: multiplying probabilities with firing rates
-for i in range(len(current_stp_traces)):
-    if times[i] <= 200*rt_scaling:
-        current_stp_traces[i] = current_stp_traces[i] * 25.
-    elif times[i] >= 200*rt_scaling and times[i] <= 500*rt_scaling:
-        current_stp_traces[i] = current_stp_traces[i] * 100.
-    elif times[i] >= 500*rt_scaling and times[i] <= 1000*rt_scaling:
-        current_stp_traces[i] = current_stp_traces[i] * 10.
-    else:
-        current_stp_traces[i] = current_stp_traces[i] * 40.
-
+print(spiking_frequencies[0])
+print(freq_stp_traces_fac[0])
+print(freq_stp_traces_dep[0])
+print(norm_stp_traces_fac[0])
+print(norm_stp_traces_dep[0])
 
 # plotting
 
-plt.figure(1)
+fig1, ax1 = plt.subplots()
+plt.title('facilitation')
+p1 = ax1.plot(spiking_frequencies, freq_stp_traces_fac, 'b--', label='$<P_{rel}>$')
+ax1.set_xlabel('r (Hz)')
+ax1.set_ylabel(r'$<P_{rel}>$')
+ax1.set_xlim([0,100])
+ax1.set_ylim([0,1.])
+ax2 = ax1.twinx()
+p2 = ax2.plot(spiking_frequencies, norm_stp_traces_fac, 'b', label=r'$<P_{rel}>r$ (Hz)')
+ax2.set_ylabel(r'$<P_{rel}>r$ (Hz)')
+plots = p1+p2
+labs = [plot.get_label() for plot in plots]
+ax1.legend(plots, labs, loc='upper left')
+#plt.savefig(output_path + '5_18_A-reproduction.png', format = 'png', dpi = 1200)
+plt.show()
 
-plt.plot(times, current_stp_traces)
-plt.xlabel('time (ms)')
-plt.ylabel(r'$<P_{rel}>r$ (Hz)')
-plt.axvline(x=200*rt_scaling, linestyle='dashed', color='grey')
-plt.axvline(x=500*rt_scaling, linestyle='dashed', color='grey')
-plt.axvline(x=1000*rt_scaling, linestyle='dashed', color='grey')
-plt.text(80*rt_scaling, 8, '25 Hz')
-plt.text(330*rt_scaling, 8, '100 Hz')
-plt.text(730*rt_scaling, 8, '10 Hz')
-plt.text(1080*rt_scaling, 8, '40 Hz')
-
-plt.savefig(output_path + '5_19-reproduction.png', format = 'png', dpi = 1200)
+fig2, ax3 = plt.subplots()
+plt.title('depression')
+p3 = ax3.plot(spiking_frequencies, freq_stp_traces_dep, 'b--', label='$<P_{rel}>$')
+ax3.set_xlabel('r (Hz)')
+ax3.set_ylabel(r'$<P_{rel}>$')
+ax3.set_xlim([0,100])
+ax3.set_ylim([0,1.])
+ax4 = ax3.twinx()
+p4 = ax4.plot(spiking_frequencies, norm_stp_traces_dep, 'b', label=r'$<P_{rel}>r$ (Hz)')
+ax4.set_ylabel(r'$<P_{rel}>r$ (Hz)')
+plots = p3+p4
+labs = [plot.get_label() for plot in plots]
+ax3.legend(plots, labs, loc='center right')
+#plt.savefig(output_path + '5_18_B-reproduction.png', format = 'png', dpi = 1200)
 plt.show()
 
 
