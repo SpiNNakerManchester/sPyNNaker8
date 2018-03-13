ea8f2312f1ee67d4927bc914765dc5498bafae51
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-03-13 11:55:15 +0000

    Factor the majority of version check code into a function for clarity

--- spynnaker8/utilities/version_util.py
+++ spynnaker8/utilities/version_util.py
@@ -1,32 +1,51 @@
 from pyNN import __version__ as pynn_version
 from neo import __version__ as neo_version
 from distutils.version import StrictVersion
+from six import raise_from
 
-_msg = "pyNN version {} and neo version {} are not supported together.\n" \
-       "Tested combinations are:\n" \
-       "\tpyNN 0.8.3 and neo 0.4.0 or\n" \
-       "\tpyNN 0.8.3 and neo 0.4.1 or\n" \
-       "\tpyNN 0.9.1 and neo 0.5.2".format(pynn_version, neo_version)
-_pynn_ver = StrictVersion(pynn_version)
-_neo_ver = StrictVersion(neo_version)
 
-print("Detected pynn version {} and neo version {}".format(
+def detect_supported_configuration(pynn_version, neo_version):
+    """ Check if the version configuration of PyNN and Neo is one we support.
+
+    .. note::
+        We strongly encourage the use of PyNN 0.9 and Neo 0.5.
+
+    :return: True if we're using old PyNN 0.8 syntax
+    :raises ImportError: If a truly unsupported system is present or if we\
+        cannot parse the version numbers (shouldn't happen)
+    """
+    try:
+        pynn = StrictVersion(pynn_version)
+        neo = StrictVersion(neo_version)
+    except Exception as e:
+        raise_from(ImportError("couldn't parse version numbers"), e)
+
+    _msg = "pyNN version {} and neo version {} are not supported together.\n" \
+           "Tested combinations are:\n" \
+           "\tpyNN 0.8.3 and neo 0.4.0 or\n" \
+           "\tpyNN 0.8.3 and neo 0.4.1 or\n" \
+           "\tpyNN 0.9.1 and neo 0.5.2".format(pynn, neo)
+
+    if pynn >= "0.9":
+        if neo < "0.5":
+            raise ImportError(_msg)
+        return False
+    elif pynn >= "0.8":
+        if neo < "0.3" or neo >= "0.5":
+            raise ImportError(_msg)
+        print("WARNING: PyNN 0.8 is deprecated at the request of the PyNN "
+              "team.")
+        print("Please upgrade your PyNN and Neo installations to at least "
+              "PyNN 0.9.2 and Neo 0.5.2")
+        print("If you specifically need PyNN 0.8 please contact us *URGENTLY* "
+              "otherwise PyNN 0.8 will be removed with the next sub-release "
+              "(i.e., before PyNN 0.7 support is removed).")
+        return True
+    else:
+        # Really old version of PyNN?! How...
+        raise ImportError(_msg)
+
+
+print("Detected PyNN version {} and Neo version {}".format(
     pynn_version, neo_version))
-
-if _pynn_ver >= "0.9":
-    if _neo_ver < "0.5":
-        raise ImportError(_msg)
-    pynn8_syntax = False
-elif _pynn_ver >= "0.8":
-    if _neo_ver < "0.3" or _neo_ver >= "0.5":
-        raise ImportError(_msg)
-    pynn8_syntax = True
-    print("WARNING: PyNN 0.8 is deprecated at the request of the PyNN team.")
-    print("Please upgrade your PyNN and Neo installations to at least "
-          "PyNN 0.9.2 and Neo 0.5.2")
-    print("If you specifically need PyNN 0.8 please contact us *URGENTLY* "
-          "otherwise PyNN 0.8 will be removed with the next sub-release. "
-          "(Before PyNN 0.7")
-else:
-    # Really old version of PyNN?! How...
-    raise ImportError(_msg)
+pynn8_syntax = detect_supported_configuration(pynn_version, neo_version)
