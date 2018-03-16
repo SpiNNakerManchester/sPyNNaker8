62df790333e5fa0e819faf6ea849edb4952059be
366anna <366anna@gmail.com>
2018-03-16 21:18:33 +0100

    Experimenting with future classifier network

--- unittests/model_tests/fusi_classifier_1.py
+++ unittests/model_tests/fusi_classifier_1.py
@@ -1,5 +1,6 @@
 import spynnaker8 as p
 from pyNN.utility.plotting import Figure, Panel, DataTable
+from pyNN.random import RandomDistribution
 import matplotlib.pyplot as plt
 import numpy as np
 from neo.core.spiketrain import SpikeTrain
@@ -20,9 +21,23 @@ p.setup(1)
 
 inp_nrn = 2000
 inh_nrn = 1000
-inp_inh_conn_prob = 0.0/inp_nrn
-w0=1
-w_mult=2.0
+inp_inh_conn_prob = 8.0/inh_nrn
+
+w0 = 0.51
+w_mult=0.1   # this parameters scales all weight variables, actual w_max = w_mult; should be 0.1-0.125 or smaller to avoid distortions in results
+a_plus = 0.11
+a_minus = 0.11
+w_min = 0.0
+w_max = 1.0
+w_drift = .0035
+th_w = 0.50
+V_th = -56.0
+Ca_th_l = 3.0
+Ca_th_h1 = 4.0
+Ca_th_h2 = 13.0
+
+
+
 #w_mult = w_mult/inp_nrn
 #simtime=300
 
@@ -34,21 +49,26 @@ w_mult=2.0
 
 pop_src = p.Population(inp_nrn, p.SpikeSourcePoisson(rate=10), label="src")
 #pop_inp = p.Population(inp_nrn, p.IF_curr_exp(), label="inp")
-pop_teacher = p.Population(1, p.SpikeSourcePoisson(rate=100), label="teacher")
+pop_teacher = p.Population(1, p.SpikeSourcePoisson(rate=50), label="teacher")
 cell_params = {"i_offset":0.0,  "tau_ca2":150, "i_alpha":1., "i_ca2":3.,   'v_reset':-65}
 #pop_inh = p.Population(inh_nrn, p.extra_models.IFCurrExpCa2Concentration, cell_params, label="inhibitory")
 pop_inh = p.Population(inh_nrn, p.IF_curr_exp(), label="inhibitory")
 pop_ex = p.Population(1, p.extra_models.IFCurrExpCa2Concentration, cell_params, label="test")
 #pop_ex = p.Population(1, p.IF_curr_exp(), label="test")
 
-rates = [10.0]*inp_nrn
-rates[0]=50
-rates[10] = 100
+rates = [2]*inp_nrn
+for i in range(100):
+    rates[i]=50
 pop_src.set(rate=rates)
 
+weights = [1]*inp_nrn
+for i in range(1000):
+     rates[i+50]=0.0*w_mult
+rd = RandomDistribution('uniform', (0, w_mult))
+
 syn_plas = p.STDPMechanism(
-     timing_dependence = p.PreOnly(A_plus = 0.15*w_max*w_mult, A_minus = 0.15*w_max*w_mult, th_v_mem=V_th, th_ca_up_l = Ca_th_l, th_ca_up_h = Ca_th_h2, th_ca_dn_l = Ca_th_l, th_ca_dn_h = Ca_th_h1),
-        weight_dependence = p.WeightDependenceFusi(w_min=w_min*w_mult, w_max=w_max*w_mult, w_drift=w_drift*w_mult, th_w=th_w * w_mult), weight=w0*w_mult, delay=1.0)
+     timing_dependence = p.PreOnly(A_plus = a_plus*w_max*w_mult, A_minus = a_minus*w_max*w_mult, th_v_mem=V_th, th_ca_up_l = Ca_th_l, th_ca_up_h = Ca_th_h2, th_ca_dn_l = Ca_th_l, th_ca_dn_h = Ca_th_h1),
+        weight_dependence = p.WeightDependenceFusi(w_min=w_min*w_mult, w_max=w_max*w_mult, w_drift=w_drift*w_mult, th_w=th_w * w_mult), weight=rd, delay=1.0)
 
 #syn = p.StaticSynapse(weight=1.0, delay=1.0)
 
@@ -62,9 +82,9 @@ proj = p.Projection(
 proj_inp_inh = p.Projection(pop_src,  pop_inh,  p.FixedProbabilityConnector(inp_inh_conn_prob),
                synapse_type=p.StaticSynapse(weight=1.0),  receptor_type='excitatory')
 proj_inh_ex = p.Projection(pop_inh,  pop_ex,  p.AllToAllConnector(),
-               synapse_type=p.StaticSynapse(weight=1.0),  receptor_type='inhibitory')
+               synapse_type=p.StaticSynapse(weight=w_mult),  receptor_type='inhibitory')
 proj_teach_ex = p.Projection(pop_teacher,  pop_ex,  p.AllToAllConnector(),
-               synapse_type=p.StaticSynapse(weight=1.0),  receptor_type='excitatory')
+               synapse_type=p.StaticSynapse(weight=2.0),  receptor_type='excitatory')
 
 
 
@@ -136,7 +156,6 @@ else:
           yticks=True, markersize=0.5, xlim=(0, plot_time), xticks=True,  data_labels=['post-spikes']),
     title="fusi spikes",
     annotations="Simulated with {}".format(p.name()))
-plt.show()
 
 #spike train is not written to the file???
 io = PyNNTextIO(filename="data/data.txt")
@@ -148,4 +167,5 @@ pop_src.write_data(io)
 io = AsciiSpikeTrainIO(filename="data/post_spikes.txt")
 pop_ex.write_data(io)
 p.end()
+plt.show()
 print "\n job done"
