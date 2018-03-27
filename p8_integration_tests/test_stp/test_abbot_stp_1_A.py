6c78cdb1d92e9ef49eb140249691a44e8a85a122
ASUS <ASUS@Lultra.manchester.ac.uk>
2018-03-27 17:16:41 +0100

    update

--- p8_integration_tests/test_stp/test_abbot_stp_1_A.py
+++ p8_integration_tests/test_stp/test_abbot_stp_1_A.py
@@ -20,48 +20,82 @@ n_exc = int(round(n_neurons * 0.8))
 n_inh = int(round(n_neurons * 0.2))
 
 
+#current = 15
+#current = RandomDistribution("uniform", low=14.625, high=15.375)
+current = RandomDistribution("uniform", low=14.95, high=15.05)
+
+
 # Neuron populations
 pop_exc = p.Population(
             n_exc,
             p.IF_curr_exp(tau_m=30, cm=30,
                           v_rest=0, v_reset=13.5, v_thresh=15,
                           tau_syn_E=3, tau_syn_I=3, tau_refrac=3,
-                          i_offset=RandomDistribution("uniform", low=14.95, high=15.05)),
-                          #i_offset=RandomDistribution("uniform", low=14.625, high=15.375)),
+                          i_offset=current),
             label="Excitatory")
 pop_inh = p.Population(
             n_inh,
             p.IF_curr_exp(tau_m=30, cm=30,
                           v_rest=0, v_reset=13.5, v_thresh=15,
                           tau_syn_E=3, tau_syn_I=3, tau_refrac=2,
-                          i_offset=RandomDistribution("uniform", low=14.95, high=15.05)),
-                          #i_offset=RandomDistribution("uniform", low=14.625, high=15.375)),
+                          i_offset=current),
             label="Inhibitory")
 
 
 #p.set_number_of_neurons_per_core(p.IF_curr_exp, 32)
 
+# Parameters for the synapses
+Aee = 1.8
+Aei = 5.4
+Aii = 7.2
+Aie = 7.2
+
+Uee = 0.5
+Uei = 0.5
+Uii = 0.04
+Uie = 0.04
+
+tau_rec_ee = 800.0
+tau_rec_ei = 800.0
+#tau_rec_ii = 100.0
+#tau_rec_ie = 100.0
+
+tau_facil_ii = 1000.0
+tau_facil_ie = 1000.0
+
+#synapse_delay = timestep
+synapse_delay = RandomDistribution("uniform", low=0, high=2)
+
+
 # Create synapse dynamics
 depress_syn_plas_ee = p.STDPMechanism(
                             timing_dependence=p.AbbotSTP(
-                                0, 0.5, 1.0, 800), # STP_type, f, P_baseline, tau_P
+                                0, Uee, 1.0, tau_rec_ee), # STP_type, f, P_baseline, tau_P
                             weight_dependence=p.STPOnlyWeightDependence(),
-                            weight=1.8, delay=timestep)
+                            weight=RandomDistribution("normal_clipped",
+                                                      mu=Aee, sigma=Aee/2.0, high=2.0*Aee, low=0.2*Aee),
+                            delay=synapse_delay)
 depress_syn_plas_ei = p.STDPMechanism(
                             timing_dependence=p.AbbotSTP(
-                                0, 0.5, 1.0, 800), # STP_type, f, P_baseline, tau_P
+                                0, Uei, 1.0, tau_rec_ei), # STP_type, f, P_baseline, tau_P
                             weight_dependence=p.STPOnlyWeightDependence(),
-                            weight=5.4, delay=timestep)
+                            weight=RandomDistribution("normal_clipped",
+                                                      mu=Aei, sigma=Aei/2.0, high=2.0*Aei, low=0.2*Aei),
+                            delay=synapse_delay)
 depress_syn_plas_ii = p.STDPMechanism(
                             timing_dependence=p.AbbotSTP(
-                                1, 0.04, 0.1, 1000), # STP_type, f, P_baseline, tau_P
+                                1, Uii, 0.1, tau_facil_ii), # STP_type, f, P_baseline, tau_P
                             weight_dependence=p.STPOnlyWeightDependence(),
-                            weight=7.2, delay=timestep)
+                            weight=RandomDistribution("normal_clipped",
+                                                      mu=Aii, sigma=Aii/2.0, high=2.0*Aii, low=0.2*Aii),
+                            delay=synapse_delay)
 depress_syn_plas_ie = p.STDPMechanism(
                             timing_dependence=p.AbbotSTP(
-                                1, 0.04, 0.1, 1000), # STP_type, f, P_baseline, tau_P
+                                1, Uie, 0.1, tau_facil_ie), # STP_type, f, P_baseline, tau_P
                             weight_dependence=p.STPOnlyWeightDependence(),
-                            weight=7.2, delay=timestep)
+                            weight=RandomDistribution("normal_clipped",
+                                                      mu=Aie, sigma=Aie/2.0, high=2.0*Aie, low=0.2*Aie),
+                            delay=synapse_delay)
 
 
 
@@ -79,8 +113,6 @@ p.Projection(
 
 
 
-# pop_exc.initialize(v=RandomDistribution("uniform", low=13.5, high=15))
-# pop_inh.initialize(v=RandomDistribution("uniform", low=13.5, high=15))
 pop_exc.initialize(v=RandomDistribution("uniform", low=0, high=15))
 pop_inh.initialize(v=RandomDistribution("uniform", low=0, high=15))
 
@@ -99,13 +131,32 @@ inh_spikes = data_inh.segments[0].spiketrains
 
 end_time = p.get_current_time()
 
+
+# sort the neurons by spiking frequency
+sorted_indices = []
+for i in range(0, len(exc_spikes)):
+    sorted_indices.append((len(exc_spikes[i]), i))
+def getKey(item):
+    return item[0]
+sorted_indices = sorted(sorted_indices, key=getKey)
+sorted_exc_spikes = []
+for i in range(0, len(exc_spikes)):
+    sorted_exc_spikes.append(exc_spikes[sorted_indices[i][1]])
+for i in range(0, len(sorted_exc_spikes)):
+    #sorted_exc_spikes[i].annotations['source_id'] = i
+    sorted_exc_spikes[i].annotations['source_index'] = i
+
 #p.end()
 
 
 Figure(
     # raster plot of the presynaptic neuron spike times
     Panel(exc_spikes,
+          #sorted_exc_spikes,
           yticks=True, markersize=2.0, xlim=(0, end_time)),
+#     Panel(inh_spikes,
+#           #sorted_inh_spikes,
+#           yticks=True, markersize=2.0, xlim=(0, end_time)),
     title="network simulation",
     annotations="Simulated with {}".format(p.name())
 )
