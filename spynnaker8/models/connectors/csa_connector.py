b4260d0d764e98b5e02f58555680a8f4cbe879c0
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-04-04 11:58:23 +0100

    Create CSAConnector class in sPyNNaker8

--- spynnaker8/models/connectors/csa_connector.py
+++ spynnaker8/models/connectors/csa_connector.py
@@ -1,12 +1,19 @@
 from spynnaker.pyNN.models.neural_projections.connectors \
-    import AbstractConnector
-from pyNN.connectors import CSAConnector as PyNNCSAConnector
+    import CSAConnector as CommonCSAConnector
 
 
-class CSAConnector(AbstractConnector, PyNNCSAConnector):
+class CSAConnector(CommonCSAConnector):
+    """
+    Create an CSA (Connection Set Algebra, Djurfeldt 2012) connector.
 
-    def __init__(self, cset, safe=True, callback=None):
-        AbstractConnector.__init__(self, safe=safe)
-        PyNNCSAConnector.__init__(
-            self, cset=cset, safe=safe, callback=callback)
-        raise NotImplementedError
+    :param csa: a csa description of the connections
+    :type csa: string
+    """
+    __slots__ = []
+
+    def __init__(
+            self, csa, safe=True, callback=None, verbose=False):
+        # pylint: disable=too-many-arguments
+        super(CSAConnector, self).__init__(
+            array=csa,
+            safe=safe, callback=callback, verbose=verbose)
