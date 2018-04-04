07c52fddfe0bb6d0515a7c4fe75815e5f04baad8
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-04-04 12:00:51 +0100

    Name the parameters correctly!

--- spynnaker8/models/connectors/csa_connector.py
+++ spynnaker8/models/connectors/csa_connector.py
@@ -6,14 +6,14 @@ class CSAConnector(CommonCSAConnector):
     """
     Create an CSA (Connection Set Algebra, Djurfeldt 2012) connector.
 
-    :param csa: a csa description of the connections
-    :type csa: string
+    :param cset: a connection set description
+    :type cset: string
     """
     __slots__ = []
 
     def __init__(
-            self, csa, safe=True, callback=None, verbose=False):
+            self, cset, safe=True, callback=None, verbose=False):
         # pylint: disable=too-many-arguments
         super(CSAConnector, self).__init__(
-            array=csa,
+            cset=cset,
             safe=safe, callback=callback, verbose=verbose)
