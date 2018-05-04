2edfa3328e724a72f91876b86c22648a9deb54fb
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-05-04 09:51:49 +0100

    changes for https://github.com/SpiNNakerManchester/sPyNNaker8/issues/136

--- p8_integration_tests/test_various/test_singleboard_spike_output.py
+++ p8_integration_tests/test_various/test_singleboard_spike_output.py
@@ -3,18 +3,20 @@ from p8_integration_tests.base_test_case import BaseTestCase
 import unittest
 
 
-class TestMultiBoardSpikeOutput(BaseTestCase):
+class TestSingleBoardSpikeOutput(BaseTestCase):
 
     counts = None
 
     @staticmethod
     def spike_receiver(label, time, neuron_ids):
-        TestMultiBoardSpikeOutput.counts[label] += len(neuron_ids)
+        TestSingleBoardSpikeOutput.counts[label] += len(neuron_ids)
 
-    def test_multi_board_spike_output(self):
-        self.assert_not_spin_three()
-        TestMultiBoardSpikeOutput.counts = dict()
-        p.setup(1.0, n_chips_required=((48 * 2) + 1))
+    @unittest.skip(
+        "https://github.com/SpiNNakerManchester/sPyNNaker8/issues/136")
+    def test_single_board_spike_output(self):
+
+        TestSingleBoardSpikeOutput.counts = dict()
+        p.setup(1.0, n_chips_required=1)
         machine = p.get_machine()
         labels = list()
         for chip in machine.ethernet_connected_chips:
@@ -26,21 +28,21 @@ class TestMultiBoardSpikeOutput(BaseTestCase):
                                label=label)
             pop.add_placement_constraint(chip.x, chip.y)
             p.external_devices.activate_live_output_for(pop)
-            TestMultiBoardSpikeOutput.counts[label] = 0
+            TestSingleBoardSpikeOutput.counts[label] = 0
 
         live_output = p.external_devices.SpynnakerLiveSpikesConnection(
             receive_labels=labels)
         for label in labels:
             live_output.add_receive_callback(
-                label, TestMultiBoardSpikeOutput.spike_receiver)
+                label, TestSingleBoardSpikeOutput.spike_receiver)
 
         p.run(1000)
         p.end()
 
         for label in labels:
             print("Received {} of 1000 spikes from {}".format(
-                TestMultiBoardSpikeOutput.counts[label], label))
-            self.assertEqual(TestMultiBoardSpikeOutput.counts[label], 1000)
+                TestSingleBoardSpikeOutput.counts[label], label))
+            self.assertEqual(TestSingleBoardSpikeOutput.counts[label], 1000)
 
 
 if __name__ == '__main__':
--- p8_integration_tests/test_various/test_spike_io_multi_run.py
+++ p8_integration_tests/test_various/test_spike_io_multi_run.py
@@ -222,8 +222,8 @@ def do_run():
 
 class SpikeIoMultiRun(BaseTestCase):
 
-    @unittest.skip("https://github.com/SpiNNakerManchester/sPyNNaker8/issues/"
-                   "83")
+    @unittest.skip(
+        "https://github.com/SpiNNakerManchester/sPyNNaker8/issues/136")
     def test_run(self):
         (spikes_forward, spikes_backward) = do_run()
         self.assertEqual(600, len(spikes_forward))
