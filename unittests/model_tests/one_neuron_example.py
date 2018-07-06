22f489a963511aaf395f9d325a3c8b74691bcb4f
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-07-06 15:45:28 +0100

    Add extra synapses

--- unittests/model_tests/one_neuron_example.py
+++ unittests/model_tests/one_neuron_example.py
@@ -18,10 +18,10 @@ neuron_params = {
 
 # pop_exc = p.Population(1, p.Izhikevich(**neuron_params),  label="LIF Neuron")
 
-pop_exc = p.Population(1, p.extra_models.IFCurrCombExp2E2I(**neuron_params),  label="LIF Neuron")
+pop_exc = p.Population(17, p.extra_models.IFCurrCombExp2E2I(**neuron_params),  label="LIF Neuron")
 
 
-spike_times = [10, 15, 17, 19, 21]
+spike_times = [10] # 15, 17, 19, 21]
 # Spike source to send spike via synapse
 pop_src1 = p.Population(1, p.SpikeSourceArray,
                         {'spike_times': spike_times}, label="src1")
@@ -30,7 +30,16 @@ pop_src1 = p.Population(1, p.SpikeSourceArray,
 # Create projection
 synapse = p.Projection(
     pop_src1, pop_exc, p.OneToOneConnector(0.6),
-    p.StaticSynapse(weight=2, delay=1), receptor_type="excitatory2")
+    p.StaticSynapse(weight=2, delay=10), receptor_type="excitatory")
+synapse = p.Projection(
+    pop_src1, pop_exc, p.OneToOneConnector(0.6),
+    p.StaticSynapse(weight=2, delay=40), receptor_type="excitatory2")
+synapse = p.Projection(
+    pop_src1, pop_exc, p.OneToOneConnector(0.6),
+    p.StaticSynapse(weight=2, delay=30), receptor_type="inhibitory")
+synapse = p.Projection(
+    pop_src1, pop_exc, p.OneToOneConnector(0.6),
+    p.StaticSynapse(weight=2, delay=20), receptor_type="inhibitory2")
 
 pop_src1.record('all')
 
@@ -54,9 +63,9 @@ Figure(
     Panel(exc_data.segments[0].filter(name='gsyn_exc')[0],
           ylabel="gsyn excitatory (mV)",
           data_labels=[pop_exc.label], yticks=True, xlim=(0, runtime)),
-#     Panel(exc_data.segments[0].filter(name='gsyn_inh')[0],
-#           ylabel="gsyn inhibitory (mV)",
-#           data_labels=[pop_src1.label], yticks=True, xlim=(0, runtime)),
+    Panel(exc_data.segments[0].filter(name='gsyn_inh')[0],
+          ylabel="gsyn inhibitory (mV)",
+          data_labels=[pop_src1.label], yticks=True, xlim=(0, runtime)),
     Panel(exc_data.segments[0].spiketrains,
           yticks=True, markersize=0.2, xlim=(0, runtime)),
     )
