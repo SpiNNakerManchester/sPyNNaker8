f5cd394235d2cd493bd1a41de3a50773b6375c78
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-07-25 12:08:12 +0100

    Update neuron profiling to use clock reads extracted from iobuf

--- p8_integration_tests/profiling/profile_neuron_update.py
+++ p8_integration_tests/profiling/profile_neuron_update.py
@@ -1,39 +1,60 @@
 import spynnaker8 as p
-import numpy
+import numpy as np
 import math
+import glob
 import unittest
 from pyNN.utility.plotting import Figure, Panel
 import matplotlib.pyplot as plt
 
-p.setup(1) # 1 for LIF; 0.1 for Izhikevich
-runtime = 1000 # Run for one second
-n_neurons = 1 # takes value 1, 2, 100, 256
-recording = False
+
+runtime = 100 # Run for one second
+n_neurons = [1, 2, 4, 8, 16, 32, 64, 128, 255] # takes value 1, 2, 100, 256
+recording = True
+
+for n in n_neurons:
+
+    p.setup(1) # 1 for LIF; 0.1 for Izhikevich
+
+    ## Create population of neurons - comments as appropriate
+    pop = p.Population(n, p.IF_curr_exp(),  label="test")
+#     pop = p.Population(n, p.IF_cond_exp(),  label="test")
+#     pop = p.Population(n, p.Izhikevich(),  label="test")
+#     pop = p.Population(n, p.extra_models.Izhikevich_cond(),  label="test")
+
+
+    # Set recording and run
+    if recording:
+        pop.record("all")
+
+    p.run(runtime)
+
+    # Extract output data
+    if recording:
+        data = pop.get_data()
+
+
+    # Read iobuf log to extract neuron update times
+    path = p.globals_variables.get_simulator()._report_default_directory + "/provenance_data/iobuf*0_0*3*"
+    diffs = {}
+    mins = {}
+    maxs = {}
+    diffs[n] = []
+    mins[n] = []
+    maxs[n] = []
+
+    # Process iobuf
+    file = open(glob.glob(path)[0])
+    for line in file.readlines():
+        if line.__contains__("Diff"):
+            diffs[n].append(float(line.split(" ")[-1].split("\n")[0]))
+
+    file.close()
+
+    print "Timing measurement output:"
+    print "{},  {},  {},  {}".format(round(np.mean(diffs[n]),0), min(diffs[n]), max(diffs[n]), np.std(diffs[n]))
 
 
 
-## Create population of neurons - comments as appropriate
-pop = p.Population(n_neurons, p.IF_curr_exp(),  label="test")
-# pop = p.Population(n_neurons, p.Izhikevich(),  label="test")
-
-# Set recording and run
-if recording:
-    pop.record("all")
-
-p.run(runtime)
-
-# Extract output data
-if recording:
-    data = pop.get_data()
-
-    # Plot
-    Figure(
-        # raster plot of neuron spike times
-        Panel(data.segments[0].spiketrains,
-              yticks=True, markersize=0.2, xlim=(0, runtime)),
-    )
-    plt.show()
-
-p.end()
+    p.end()
 
 
