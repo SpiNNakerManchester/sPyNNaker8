d31c87293987f286549e8b380823364cb69a165d
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-07-27 16:42:29 +0100

    Combined variable poisson source in to standard one

--- spynnaker8/__init__.py
+++ spynnaker8/__init__.py
@@ -94,6 +94,9 @@ from spynnaker8.models.model_data_holders.spike_source_array_data_holder \
 # noinspection PyUnresolvedReferences
 from spynnaker8.models.model_data_holders.spike_source_poisson_data_holder \
     import SpikeSourcePoissonDataHolder as SpikeSourcePoisson
+from spynnaker8.models.model_data_holders\
+    .spike_source_poisson_variable_data_holder \
+    import SpikeSourcePoissonVariableDataHolder as SpikeSourcePoissonVariable
 
 # pops
 # noinspection PyUnresolvedReferences
@@ -140,6 +143,7 @@ __all__ = [
     # neuron stuff
     'IF_cond_exp', 'IF_curr_exp', "IF_curr_alpha",
     'Izhikevich', 'SpikeSourceArray', 'SpikeSourcePoisson',
+    'SpikeSourcePoissonVariable',
     # pops
     'Assembly', 'Population', 'PopulationView',
     # projection
