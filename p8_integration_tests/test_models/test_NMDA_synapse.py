3d1505972aba9b34cc87c0ce5f40c1bbc805a03a
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-09-05 13:16:45 +0100

    Add voltage-gated NMDA test

--- p8_integration_tests/test_models/test_NMDA_synapse.py
+++ p8_integration_tests/test_models/test_NMDA_synapse.py
@@ -4,45 +4,44 @@ import matplotlib.pyplot as plt
 
 timestep = 0.1
 p.setup(timestep)  # set simulation timestep (ms)
-runtime = 750
+runtime = 2000
 
 # Post-synapse population
+new_i_offset = -20
+num_intervals = 7
 neuron_params = {
-        # 'v_init': -65,
+        'v_init': -88.1,
         'g_Na': 0.2,
         'E_Na': 30.0,
         'g_K': 1.85,
         'E_K': -90.0,
         'tau_m': 16,
         't_spike': 2,
-        'i_offset': 35,
+        'i_offset': new_i_offset,
         }
 
 pop_exc = p.Population(1,
                        p.extra_models.HillTononiNeuron(**neuron_params),
                        label="HT Neuron")
 
-spike_times = [10, 15, 17, 170, 190, 192, 200, 205, 400, 405, 410, 415, 420,
-               425, 430, 435, 440, 721]
-# Spike source to send spike via synapse
+spike_times = [x + 50 for x in range(0, runtime, (runtime/num_intervals))]
+# # Spike source to send spike via synapse
 pop_src1 = p.Population(1, p.SpikeSourceArray,
                         {'spike_times': spike_times}, label="src1")
-
 
 # Create projection
 synapse = p.Projection(
     pop_src1, pop_exc, p.OneToOneConnector(0.6),
-    p.StaticSynapse(weight=1, delay=1), receptor_type="AMPA")
-
-# pop_src1.record('all')
+    p.StaticSynapse(weight=1, delay=1), receptor_type="NMDA")
 
 pop_exc.record("all")
-p.run(runtime)
-weights = []
+for i in range(num_intervals):
+    p.run(runtime/num_intervals)
+    new_i_offset += 10
+    pop_exc.set(i_offset=new_i_offset)
 
 # pre_spikes = pop_src1.get_data('spikes')
 exc_data = pop_exc.get_data()
-
 
 # Plot
 Figure(
@@ -53,14 +52,14 @@ Figure(
     Panel(exc_data.segments[0].filter(name='v')[0],
           ylabel="Membrane potential (mV)",
           data_labels=[pop_exc.label], yticks=True, xlim=(0, runtime)),
-    Panel(exc_data.segments[0].filter(name='gsyn_inh')[0],
-          ylabel="Threshold (mV)",
-          data_labels=[pop_exc.label], yticks=True, xlim=(0, runtime)),
-    Panel(exc_data.segments[0].filter(name='gsyn_exc')[0],
-          ylabel="gsyn excitatory (mV)",
-          data_labels=[pop_exc.label], yticks=True, xlim=(0, runtime)),
-    Panel(exc_data.segments[0].spiketrains,
-          yticks=True, markersize=0.2, xlim=(0, runtime)),
+    # Panel(exc_data.segments[0].filter(name='gsyn_inh')[0],
+    #       ylabel="Threshold (mV)",
+    #       data_labels=[pop_exc.label], yticks=True, xlim=(0, runtime)),
+    # Panel(exc_data.segments[0].filter(name='gsyn_exc')[0],
+    #       ylabel="gsyn excitatory (mV)",
+    #       data_labels=[pop_exc.label], yticks=True, xlim=(0, runtime)),
+    # Panel(exc_data.segments[0].spiketrains,
+    #       yticks=True, markersize=0.2, xlim=(0, runtime)),
     )
 
 plt.show()
