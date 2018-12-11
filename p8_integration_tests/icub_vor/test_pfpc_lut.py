e642cf978bcd91d42c94317eb5a6fa5c7c0722c3
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-12-11 10:45:09 +0000

    Update PFPC LUT test

--- p8_integration_tests/icub_vor/test_pfpc_lut.py
+++ p8_integration_tests/icub_vor/test_pfpc_lut.py
@@ -1,7 +1,17 @@
 from spynnaker.pyNN.models.neuron.plasticity.stdp.common \
     import plasticity_helpers
+import math
 
-plasticity_helpers.write_pfpc_lut(spec=None, time_constant=99, lut_size=256, shift=0, time_probe=99)
+# if we want a kernel peak at t=100ms
+# sin_power=20
+t_peak = 50
+# time_constant = 1/(math.atan(sin_power)/t_peak)
+
+plasticity_helpers.write_pfpc_lut(spec=None,
+                                  peak_time=t_peak,
+                                  lut_size=256,
+                                  shift=0,
+                                  time_probe=t_peak)
 
 
 
