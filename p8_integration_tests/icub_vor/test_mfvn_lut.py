5a3575b8da4c7a511bf59c623f555e6c9f88be82
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-12-11 11:40:49 +0000

    Add mfvn lut test and configure

--- p8_integration_tests/icub_vor/test_mfvn_lut.py
+++ p8_integration_tests/icub_vor/test_mfvn_lut.py
@@ -1,7 +1,15 @@
 from spynnaker.pyNN.models.neuron.plasticity.stdp.common \
     import plasticity_helpers
 
-plasticity_helpers.write_pfpc_lut(spec=None, time_constant=99, lut_size=256, shift=0, time_probe=99)
+beta = 10
+sigma = 200
+
+plasticity_helpers.write_mfvn_lut(spec=None,
+                                  sigma=sigma,
+                                  beta=beta,
+                                  lut_size=256,
+                                  shift=0,
+                                  time_probe=99)
 
 
 
