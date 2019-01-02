c533f7faddbc44e0dcb0c785a0fc59e346b9dd13
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2019-01-02 08:38:22 +0000

    Update test

--- p8_integration_tests/test_models/one_patch_clamped_neuron.py
+++ p8_integration_tests/test_models/one_patch_clamped_neuron.py
@@ -1,26 +1,36 @@
 import spynnaker8 as p
 from pyNN.utility.plotting import Figure, Panel
 import matplotlib.pyplot as plt
+from decimal import Clamped
 
 timestep = 0.1
 p.setup(timestep)  # set simulation timestep (ms)
 runtime = 1500
 
+
+base_potential = -65
+clamped_potential = 0
+
 neuron_params = {
-    'v': -50
+    'v': base_potential,
+    'g_H': 0,
+    'g_T': 0,
+    'g_NaP': 0.,
+    'g_DK': 0.5
     }
+
 patch_clamped_neuron = p.Population(1,
 #                         p.IF_curr_exp(),
                     p.extra_models.PatchClamped(**neuron_params),
-                    label="HT Neuron")
+                    label="Patch Clamped HT Neuron")
 
 patch_clamped_neuron.record("all")
 
 # patch_clamped_neuron.set(v=0)
 p.run(runtime/3)
-patch_clamped_neuron.set(v=10)
+patch_clamped_neuron.set(v=clamped_potential)
 p.run(runtime/3)
-patch_clamped_neuron.set(v=-50)
+patch_clamped_neuron.set(v=base_potential)
 p.run(runtime/3)
 
 # pre_spikes = pop_src1.get_data('spikes')
@@ -36,10 +46,10 @@ Figure(
           ylabel="Membrane potential (mV)",
           data_labels=[patch_clamped_neuron.label], yticks=True, xlim=(0, runtime)),
     Panel(exc_data.segments[0].filter(name='gsyn_inh')[0],
-          ylabel="Sum intrinsic currents (mV)",
+          ylabel="I_Na",
           data_labels=[patch_clamped_neuron.label], yticks=True, xlim=(0, runtime)),
     Panel(exc_data.segments[0].filter(name='gsyn_exc')[0],
-          ylabel="Threshold (mV)",
+          ylabel="I_dK",
           data_labels=[patch_clamped_neuron.label], yticks=True, xlim=(0, runtime)),
     Panel(exc_data.segments[0].spiketrains,
           yticks=True, markersize=0.2, xlim=(0, runtime)),
