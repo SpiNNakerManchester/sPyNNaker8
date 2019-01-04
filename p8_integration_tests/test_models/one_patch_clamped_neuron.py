68964afa7bbd7beb6ca5ab509f4f88539c5f5a92
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2019-01-04 09:51:54 +0000

    tidy patch clamp test

--- p8_integration_tests/test_models/one_patch_clamped_neuron.py
+++ p8_integration_tests/test_models/one_patch_clamped_neuron.py
@@ -1,7 +1,6 @@
 import spynnaker8 as p
 from pyNN.utility.plotting import Figure, Panel
 import matplotlib.pyplot as plt
-from decimal import Clamped
 
 timestep = 0.1
 p.setup(timestep)  # set simulation timestep (ms)
@@ -20,40 +19,33 @@ neuron_params = {
     }
 
 patch_clamped_neuron = p.Population(1,
-#                         p.IF_curr_exp(),
-                    p.extra_models.PatchClamped(**neuron_params),
-                    label="Patch Clamped HT Neuron")
+                                    p.extra_models.PatchClamped(
+                                        **neuron_params),
+                                    label="Patch Clamped HT Neuron")
 
 patch_clamped_neuron.record("all")
 
-# patch_clamped_neuron.set(v=0)
 p.run(runtime/3)
 patch_clamped_neuron.set(v=clamped_potential)
 p.run(runtime/3)
 patch_clamped_neuron.set(v=base_potential)
 p.run(runtime/3)
 
-# pre_spikes = pop_src1.get_data('spikes')
 exc_data = patch_clamped_neuron.get_data()
 
-# Plot
 Figure(
-    # raster plot of the presynaptic neuron spike times
-    # Panel(pre_spikes_slow.segments[0].spiketrains,
-    # yticks=True, markersize=0.2, xlim=(0, runtime)),
-    # plot data for postsynaptic neuron
     Panel(exc_data.segments[0].filter(name='v')[0],
           ylabel="Membrane potential (mV)",
-          data_labels=[patch_clamped_neuron.label], yticks=True, xlim=(0, runtime)),
+          data_labels=[patch_clamped_neuron.label], yticks=True,
+          xlim=(0, runtime)),
     Panel(exc_data.segments[0].filter(name='gsyn_inh')[0],
           ylabel="I_Na",
-          data_labels=[patch_clamped_neuron.label], yticks=True, xlim=(0, runtime)),
+          data_labels=[patch_clamped_neuron.label], yticks=True,
+          xlim=(0, runtime)),
     Panel(exc_data.segments[0].filter(name='gsyn_exc')[0],
           ylabel="I_dK",
-          data_labels=[patch_clamped_neuron.label], yticks=True, xlim=(0, runtime)),
-    Panel(exc_data.segments[0].spiketrains,
-          yticks=True, markersize=0.2, xlim=(0, runtime)),
-    )
+          data_labels=[patch_clamped_neuron.label], yticks=True,
+          xlim=(0, runtime)))
 
 plt.show()
 p.end()
