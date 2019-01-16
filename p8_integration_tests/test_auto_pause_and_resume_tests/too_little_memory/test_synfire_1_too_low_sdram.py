a646ae5a787022e2e9351d4d2211482b24bb4e78
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2019-01-16 13:35:11 +0000

    Manual Mini merge to be like Jenkins Branch

--- p8_integration_tests/test_auto_pause_and_resume_tests/too_little_memory/test_synfire_1_too_low_sdram.py
+++ p8_integration_tests/test_auto_pause_and_resume_tests/too_little_memory/test_synfire_1_too_low_sdram.py
@@ -5,7 +5,7 @@ import pytest
 
 from p8_integration_tests.base_test_case import BaseTestCase
 from p8_integration_tests.scripts.synfire_run import SynfireRunner
-from pacman.exceptions import PacmanValueError
+from pacman.exceptions import PacmanPartitionException
 
 n_neurons = 200  # number of neurons in each population
 runtime = 3000
@@ -13,17 +13,17 @@ neurons_per_core = n_neurons / 2
 synfire_run = SynfireRunner()
 
 
-class TestGsyn(BaseTestCase):
+class TestTooLow(BaseTestCase):
     """
     tests the printing of get gsyn given a simulation
     """
 
-    def test_get_gsyn(self):
-        with pytest.raises(PacmanValueError):
+    def test_too_low(self):
+        with pytest.raises(PacmanPartitionException):
             synfire_run.do_run(n_neurons, neurons_per_core=neurons_per_core,
                                run_times=[runtime])
 
 
 if __name__ == '__main__':
     synfire_run.do_run(n_neurons, neurons_per_core=neurons_per_core,
-                       run_times=[runtime])
+run_times=[runtime])
