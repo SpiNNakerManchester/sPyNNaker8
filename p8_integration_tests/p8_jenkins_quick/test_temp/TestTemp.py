70311990f8fe45fadb01dac2e6de79dcbb847847
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-02-19 16:18:24 +0000

    temp test

---
+++ p8_integration_tests/p8_jenkins_quick/test_temp/TestTemp.py
@@ -0,0 +1,15 @@
+from os.path import expanduser
+from spinn_front_end_common.utilities import globals_variables
+import spynnaker8 as sim
+from p8_integration_scripts.base_test_case import BaseTestCase
+
+
+class TestTemp(BaseTestCase):
+    sim.setup(timestep=1.0)
+    sim.run(1)
+    report_directory = globals_variables.get_simulator() \
+        ._report_default_directory
+    home = expanduser("~")
+    print(report_directory)
+    print(home)
+    sim.end()
