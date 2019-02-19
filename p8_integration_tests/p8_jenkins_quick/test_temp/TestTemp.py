9fdcbfa958835791195dabd8930c41bd8c0c8aac
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-02-19 16:39:03 +0000

    intentional test fail in a test

--- p8_integration_tests/p8_jenkins_quick/test_temp/TestTemp.py
+++ p8_integration_tests/p8_jenkins_quick/test_temp/TestTemp.py
@@ -5,12 +5,14 @@ from p8_integration_scripts.base_test_case import BaseTestCase
 
 
 class TestTemp(BaseTestCase):
-    sim.setup(timestep=1.0)
-    sim.run(1)
-    report_directory = globals_variables.get_simulator() \
-        ._report_default_directory
-    home = expanduser("~")
-    print(report_directory)
-    print(home)
-    sim.end()
-    pop = 1/0
+
+    def test_pop(self):
+        sim.setup(timestep=1.0)
+        sim.run(1)
+        report_directory = globals_variables.get_simulator() \
+            ._report_default_directory
+        home = expanduser("~")
+        print(report_directory)
+        print(home)
+        sim.end()
+        pop = 1/0
