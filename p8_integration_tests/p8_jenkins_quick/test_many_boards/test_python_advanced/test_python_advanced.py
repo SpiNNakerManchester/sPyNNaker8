f00e36e3b04aee855c0adc46be23306608229ac8
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-02-20 14:42:07 +0000

    Test python with and without advanced monitors

--- p8_integration_tests/p8_jenkins_quick/test_many_boards/test_python_advanced/test_python_advanced.py
+++ p8_integration_tests/p8_jenkins_quick/test_many_boards/test_python_advanced/test_python_advanced.py
@@ -3,11 +3,11 @@ from p8_integration_tests.base_test_case import BaseTestCase
 from p8_integration_tests.scripts.manyBoards import ManyBoards
 
 
-class TestManyBoards(BaseTestCase):
+class TestPythonAdvanced(BaseTestCase):
 
     def test_run(self):
         me = ManyBoards()
-        sim = me.do_run(n_boards=5, n_neurons=255, simtime=300)
+        sim = me.do_run(n_boards=11, n_neurons=2550, simtime=3000)
         results = self.get_run_time_of_BufferExtractor()
-        self.report(results, "manyBoards")
+        self.report(results, "python_advanced")
         sim.end()
---
+++ p8_integration_tests/p8_jenkins_quick/test_many_boards/test_python_simple/test_python_simple.py
@@ -0,0 +1,13 @@
+import spynnaker8 as sim
+from p8_integration_tests.base_test_case import BaseTestCase
+from p8_integration_tests.scripts.manyBoards import ManyBoards
+
+
+class TestPythonSimple(BaseTestCase):
+
+    def test_run(self):
+        me = ManyBoards()
+        sim = me.do_run(n_boards=11, n_neurons=2550, simtime=3000)
+        results = self.get_run_time_of_BufferExtractor()
+        self.report(results, "python_simple")
+        sim.end()
