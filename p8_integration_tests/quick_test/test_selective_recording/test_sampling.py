fb10620744863378122323b1dad4c50e112d60be
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-02-25 14:19:34 +0000

    fixed tests

--- p8_integration_tests/quick_test/test_selective_recording/test_sampling.py
+++ p8_integration_tests/quick_test/test_selective_recording/test_sampling.py
@@ -16,17 +16,16 @@ class TestSampling(BaseTestCase):
         pop = ps.create_population(sim, n_neurons=32 * 2, label="test",
                                    spike_rate=spike_rate,
                                    spike_rec_indexes=spike_rec_indexes,
-                                   v_rate=v_rate, v_rec_indexes=v_rec_indexes,
-                                   is_view=False)
+                                   v_rate=v_rate, v_rec_indexes=v_rec_indexes)
         sim.run(simtime)
         ps.check(pop, simtime,
                  spike_rate=spike_rate, spike_rec_indexes=spike_rec_indexes,
-                 v_rate=v_rate, v_rec_indexes=v_rec_indexes)
+                 v_rate=v_rate, v_rec_indexes=v_rec_indexes, is_view=False)
 
-    def test_views(self):
+    def test_multirun(self):
         ps = PatternSpiker()
         sim.setup(timestep=1)
-        simtime = 100
+        simtime = 32
         spike_rate = 5
         spike_rec_indexes = [1, 3, 5, 7, 9, 10]
         v_rec_indexes = [0, 21, 32, 45]
@@ -34,12 +33,16 @@ class TestSampling(BaseTestCase):
         pop = ps.create_population(sim, n_neurons=32 * 2, label="test",
                                    spike_rate=spike_rate,
                                    spike_rec_indexes=spike_rec_indexes,
-                                   v_rate=v_rate, v_rec_indexes=v_rec_indexes,
-                                   is_view=True)
+                                   v_rate=v_rate, v_rec_indexes=v_rec_indexes)
         sim.run(simtime)
-        ps.check(pop, simtime,
+        sim.run(simtime)
+        sim.run(simtime)
+        ps.check(pop, simtime * 3,
                  spike_rate=spike_rate, spike_rec_indexes=spike_rec_indexes,
-                 v_rate=v_rate, v_rec_indexes=v_rec_indexes)
+                 v_rate=v_rate, v_rec_indexes=v_rec_indexes, is_view=False)
+        ps.check(pop, simtime * 3,
+                 spike_rate=spike_rate, spike_rec_indexes=spike_rec_indexes,
+                 v_rate=v_rate, v_rec_indexes=v_rec_indexes, is_view=True)
 
     def test_standard(self):
         ps = PatternSpiker()
