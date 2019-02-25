7d64ae9d243a8aaaf5ba9f49f915deb0be5176b0
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-02-25 12:10:46 +0000

    Newline testing

--- p8_integration_tests/quick_test/test_temp/test_temp.py
+++ p8_integration_tests/quick_test/test_temp/test_temp.py
@@ -6,12 +6,12 @@ class TestTemp(BaseTestCase):
 
     def test_run1(self):
         sim.setup(timestep=1.0, n_chips_required=5)
-        self.report("this is a new test\n", "run1")
+        self.report("this is a new test", "run1")
         self.report("this is more stuff\n", "run1")
         sim.end()
 
     def test_run2(self):
         sim.setup(timestep=1.0, n_chips_required=5)
         self.report("this is another test\n", "run2")
-        self.report("this is extra stuff\n", "run2a")
+        self.report("this is extra stuff", "run2a")
         sim.end()
