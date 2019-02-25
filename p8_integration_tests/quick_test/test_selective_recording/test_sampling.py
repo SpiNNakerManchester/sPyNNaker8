dd4697e6c2d89c3ebdaf6a28097bd05f540b169f
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-02-25 13:58:51 +0000

    Test selective recording

---
+++ p8_integration_tests/quick_test/test_selective_recording/test_sampling.py
@@ -0,0 +1,50 @@
+import spynnaker8 as sim
+from p8_integration_tests.base_test_case import BaseTestCase
+from p8_integration_tests.scripts.patternSpiker import PatternSpiker
+
+
+class TestSampling(BaseTestCase):
+
+    def test_small(self):
+        ps = PatternSpiker()
+        sim.setup(timestep=1)
+        simtime = 100
+        spike_rate = 5
+        spike_rec_indexes = [1, 3, 5, 7, 9, 10]
+        v_rec_indexes = [0, 21, 32, 45]
+        v_rate = 3
+        pop = ps.create_population(sim, n_neurons=32 * 2, label="test",
+                                   spike_rate=spike_rate,
+                                   spike_rec_indexes=spike_rec_indexes,
+                                   v_rate=v_rate, v_rec_indexes=v_rec_indexes,
+                                   is_view=False)
+        sim.run(simtime)
+        ps.check(pop, simtime,
+                 spike_rate=spike_rate, spike_rec_indexes=spike_rec_indexes,
+                 v_rate=v_rate, v_rec_indexes=v_rec_indexes)
+
+    def test_views(self):
+        ps = PatternSpiker()
+        sim.setup(timestep=1)
+        simtime = 100
+        spike_rate = 5
+        spike_rec_indexes = [1, 3, 5, 7, 9, 10]
+        v_rec_indexes = [0, 21, 32, 45]
+        v_rate = 3
+        pop = ps.create_population(sim, n_neurons=32 * 2, label="test",
+                                   spike_rate=spike_rate,
+                                   spike_rec_indexes=spike_rec_indexes,
+                                   v_rate=v_rate, v_rec_indexes=v_rec_indexes,
+                                   is_view=True)
+        sim.run(simtime)
+        ps.check(pop, simtime,
+                 spike_rate=spike_rate, spike_rec_indexes=spike_rec_indexes,
+                 v_rate=v_rate, v_rec_indexes=v_rec_indexes)
+
+    def test_standard(self):
+        ps = PatternSpiker()
+        sim.setup(timestep=1)
+        simtime = 100
+        pop = ps.create_population(sim, n_neurons=32 * 4, label="test")
+        sim.run(simtime)
+        ps.check(pop, simtime)
