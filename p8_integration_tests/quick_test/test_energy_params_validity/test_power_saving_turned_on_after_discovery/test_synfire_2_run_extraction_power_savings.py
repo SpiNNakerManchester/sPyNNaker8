aec1f1a22b71c6a2f0e8a2a1e677aac111d3fdcf
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-02-28 14:35:01 +0000

    power on off set simplified

---
+++ p8_integration_tests/quick_test/test_energy_params_validity/test_power_saving_turned_on_after_discovery/test_synfire_2_run_extraction_power_savings.py
@@ -0,0 +1,17 @@
+from testfixtures import LogCapture
+import spynnaker8 as sim
+from p8_integration_tests.base_test_case import BaseTestCase
+
+
+class Synfire2RunExtractionIfCurrExp(BaseTestCase):
+    def test_run(self):
+        with LogCapture() as lc:
+            sim.setup(1.0)
+            pop = sim.Population(1, sim.IF_curr_exp, {}, label="pop")
+            inp = sim.Population(1, sim.SpikeSourceArray(
+                spike_times=[0]), label="input")
+            sim.Projection(inp, pop, sim.OneToOneConnector(),
+                           synapse_type=sim.StaticSynapse(weight=5))
+            sim.run(10)
+            self.assert_logs_messages(
+                lc.records, "Working out if machine is booted", 'INFO', 2)
