51d859e72413024de2f9fc0a744dc45c026ad59c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-03-02 10:43:08 +0000

    Minor fix to allow running

--- unittests/test_integration_using_virtual_baord/test_SA_neuron_class_no_edge.py
+++ unittests/test_integration_using_virtual_baord/test_SA_neuron_class_no_edge.py
@@ -4,6 +4,9 @@ from p8_integration_tests.base_test_case import BaseTestCase
 
 class SSANeuronClassNoEdgeTest(BaseTestCase):
 
+    def __init__(self):
+        BaseTestCase.__init__(self, methodName="test_run")
+
     def test_run(self):
         sim.setup()
 
