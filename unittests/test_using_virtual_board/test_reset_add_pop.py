54f7e831e53b0f614029aae5a4bb1c855da01d0b
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-03-18 17:01:09 +0000

    cleaner multirun tests

---
+++ unittests/test_using_virtual_board/test_reset_add_pop.py
@@ -0,0 +1,19 @@
+import spynnaker8 as sim
+from p8_integration_tests.base_test_case import BaseTestCase
+
+
+class TestResetAdd(BaseTestCase):
+
+    def testReset_add(self):
+        sim.setup(timestep=1.0)
+        sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 1)
+
+        input = sim.Population(
+            1, sim.SpikeSourceArray(spike_times=[0]), label="input")
+        pop_1 = sim.Population(2, sim.IF_curr_exp(), label="pop_1")
+        sim.Projection(input, pop_1, sim.AllToAllConnector(),
+                       synapse_type=sim.StaticSynapse(weight=5, delay=1))
+        sim.run(10)
+        pop_2 = sim.Population(2, sim.IF_curr_exp(), label="pop_2")
+        with self.assertRaises(NotImplementedError):
+            sim.run(10)
