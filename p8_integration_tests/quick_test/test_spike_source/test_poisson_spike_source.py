c7352e924ab7dbd7bafa367bc83a359f944fe4c6
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-04-05 11:54:41 +0100

    test for rates

--- p8_integration_tests/quick_test/test_spike_source/test_poisson_spike_source.py
+++ p8_integration_tests/quick_test/test_spike_source/test_poisson_spike_source.py
@@ -1,6 +1,6 @@
 from __future__ import division
 from p8_integration_tests.base_test_case import BaseTestCase
-import spynnaker8 as p
+import spynnaker8 as sim
 
 
 class MyTestCase(BaseTestCase):
@@ -11,12 +11,13 @@ class MyTestCase(BaseTestCase):
         count = 0
         for a_spikes in spikes:
             count += len(a_spikes)
-        self.assertAlmostEqual(expected, count/len(spikes), delta=expected/10)
+        self.assertAlmostEqual(expected, count/len(spikes), delta=expected/10,
+                               msg="Errror on {}".format(input.label))
 
     def recording_poisson_spikes(self):
-        p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
-        n_neurons = 256  # number of neurons in each population
-        p.set_number_of_neurons_per_core(p.IF_curr_exp, n_neurons / 2)
+        sim.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
+        n_neurons = 200  # number of neurons in each population
+        sim.set_number_of_neurons_per_core(sim.IF_curr_exp, n_neurons / 2)
 
         cell_params_lif = {'cm': 0.25,
                            'i_offset': 0.0,
@@ -29,27 +30,27 @@ class MyTestCase(BaseTestCase):
                            'v_thresh': -50.0
                            }
 
-        pop_1 = p.Population(
-            n_neurons, p.IF_curr_exp, cell_params_lif, label='pop_1')
-        input = p.Population(
-            n_neurons, p.SpikeSourcePoisson, {}, label='inputSpikes_1')
+        pop_1 = sim.Population(
+            n_neurons, sim.IF_curr_exp, cell_params_lif, label='pop_1')
+        input = sim.Population(
+            n_neurons, sim.SpikeSourcePoisson, {}, label='inputSpikes_1')
 
-        p.Projection(input, pop_1, p.OneToOneConnector())
+        sim.Projection(input, pop_1, sim.OneToOneConnector())
 
         input.record("spikes")
 
-        p.run(5000)
+        sim.run(5000)
         self.check_spikes(input, 5)
 
-        p.end()
+        sim.end()
 
     def test_recording_poisson_spikes(self):
         self.runsafe(self.recording_poisson_spikes)
 
     def recording_poisson_spikes_big(self):
-        p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
+        sim.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
         n_neurons = 2560  # number of neurons in each population
-        p.set_number_of_neurons_per_core(p.IF_curr_exp, n_neurons / 2)
+        sim.set_number_of_neurons_per_core(sim.IF_curr_exp, n_neurons / 2)
 
         cell_params_lif = {'cm': 0.25,
                            'i_offset': 0.0,
@@ -62,27 +63,27 @@ class MyTestCase(BaseTestCase):
                            'v_thresh': -50.0
                            }
 
-        pop_1 = p.Population(
-            n_neurons, p.IF_curr_exp, cell_params_lif, label='pop_1')
-        input = p.Population(
-            n_neurons, p.SpikeSourcePoisson, {}, label='inputSpikes_1')
+        pop_1 = sim.Population(
+            n_neurons, sim.IF_curr_exp, cell_params_lif, label='pop_1')
+        input = sim.Population(
+            n_neurons, sim.SpikeSourcePoisson, {}, label='inputSpikes_1')
 
-        p.Projection(input, pop_1, p.OneToOneConnector())
+        sim.Projection(input, pop_1, sim.OneToOneConnector())
 
         input.record("spikes")
 
-        p.run(5000)
+        sim.run(5000)
         self.check_spikes(input, 5)
 
-        p.end()
+        sim.end()
 
     def test_recording_poisson_spikes_big(self):
         self.runsafe(self.recording_poisson_spikes_big)
 
     def recording_poisson_spikes_rate_0(self):
-        p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
+        sim.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
         n_neurons = 256  # number of neurons in each population
-        p.set_number_of_neurons_per_core(p.IF_curr_exp, n_neurons / 2)
+        sim.set_number_of_neurons_per_core(sim.IF_curr_exp, n_neurons / 2)
 
         cell_params_lif = {'cm': 0.25,
                            'i_offset': 0.0,
@@ -95,19 +96,56 @@ class MyTestCase(BaseTestCase):
                            'v_thresh': -50.0
                            }
 
-        pop_1 = p.Population(
-            n_neurons, p.IF_curr_exp, cell_params_lif, label='pop_1')
-        input = p.Population(
-            n_neurons, p.SpikeSourcePoisson, {'rate': 0}, label='input')
+        pop_1 = sim.Population(
+            n_neurons, sim.IF_curr_exp, cell_params_lif, label='pop_1')
+        input = sim.Population(
+            n_neurons, sim.SpikeSourcePoisson, {'rate': 0}, label='input')
 
-        p.Projection(input, pop_1, p.OneToOneConnector())
+        sim.Projection(input, pop_1, sim.OneToOneConnector())
 
         input.record("spikes")
 
-        p.run(5000)
+        sim.run(5000)
         self.check_spikes(input, 0)
 
-        p.end()
+        sim.end()
 
     def test_recording_poisson_spikes_rate_0(self):
         self.runsafe(self.recording_poisson_spikes_rate_0)
+
+    def check_rates(self, rates, seconds):
+        n_neurons = 100
+        sim.setup(timestep=1.0)
+        inputs = {}
+        for rate in rates:
+            params = {"rate": rate}
+            input = sim.Population(
+                n_neurons, sim.SpikeSourcePoisson, params,
+                label='inputSpikes_{}'.format(rate))
+            input.record("spikes")
+            inputs[rate] = input
+        sim.run(seconds * 1000)
+        for rate in rates:
+            self.check_spikes(inputs[rate], rate*seconds)
+        sim.end()
+
+    def recording_poisson_spikes_rate_fast(self):
+        self.check_rates(
+            [10.24, 20.48, 40.96, 81.92, 163.84, 327.68, 655.36, 1310.72], 10)
+
+    def test_recording_poisson_spikes_rate_fast(self):
+        self.runsafe(self.recording_poisson_spikes_rate_fast)
+
+    def recording_poisson_spikes_rate_slow(self):
+        try:
+            self.check_rates(
+                [0, 0.01, 0.02, 0.04, 0.08, 0.16, 0.32,
+                 0.64, 1.28, 2.56, 5.12],
+                100)
+        except AssertionError:
+            sim.end()
+            self.known_issue(
+                "https://github.com/SpiNNakerManchester/sPyNNaker/issues/629")
+
+    def test_recording_poisson_spikes_rate_slow(self):
+        self.runsafe(self.recording_poisson_spikes_rate_slow)
