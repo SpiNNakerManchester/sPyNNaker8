691703379818dee3118f91bb89ad5e50334f60ae
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-04-10 11:51:05 +0100

    removed incorrect duplicate code

--- p8_integration_tests/scripts_test/examples_header.py
+++ p8_integration_tests/scripts_test/examples_header.py
@@ -1,8 +1,5 @@
 import os
 from p8_integration_tests.base_test_case import BaseTestCase
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt  # noqa: E401
 
 
 class TestExamples(BaseTestCase):
@@ -18,26 +15,5 @@ class TestExamples(BaseTestCase):
         # Jenkins appears to place "PyNN8Examples" here
         if not os.path.exists(self._introlab_dir):
             parent_dir = os.path.dirname(spynnaker8_dir)
+
             self._introlab_dir = os.path.join(parent_dir, "PyNN8Examples")
-
-    def mockshow(self):
-        self._show = True
-
-    def check_script(self, script, broken):
-        plotting = "import matplotlib.pyplot" in open(script).read()
-        if plotting:
-            self._show = False
-            plt.show = self.mockshow
-        from runpy import run_path
-        try:
-            run_path(script)
-            if plotting:
-                assert self._show
-            self.report(script, "scripts_ran_successfully")
-        except Exception as ex:
-            if broken:
-                self.report(
-                    script, "scripts_skipped_with_unkown_issues")
-            else:
-                print("Error on {}".format(script))
-                raise ex
