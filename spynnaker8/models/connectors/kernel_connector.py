fade4aa48f215db9bc331adda9c84349985271e7
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-04-26 09:49:47 +0100

    Allow user to pass in weight kernel and delay kernel

--- spynnaker8/models/connectors/kernel_connector.py
+++ spynnaker8/models/connectors/kernel_connector.py
@@ -12,12 +12,12 @@ class KernelConnector(CommonKernelConnector):
     __slots__ = []
 
     def __init__(
-            self, shape_pre, shape_post, shape_kernel,
-            shape_common=None, pre_sample_steps=None, pre_start_coords=None,
-            post_sample_steps=None, post_start_coords=None,
-            safe=True, space=None, verbose=False):
+            self, shape_pre, shape_post, shape_kernel, weight_kernel=None,
+            delay_kernel=None, shape_common=None, pre_sample_steps=None,
+            pre_start_coords=None, post_sample_steps=None,
+            post_start_coords=None, safe=True, space=None, verbose=False):
         # pylint: disable=too-many-arguments
         super(KernelConnector, self).__init__(
-            shape_pre, shape_post, shape_kernel,
-            shape_common, pre_sample_steps, pre_start_coords,
+            shape_pre, shape_post, shape_kernel, weight_kernel,
+            delay_kernel, shape_common, pre_sample_steps, pre_start_coords,
             post_sample_steps, post_start_coords, safe, space, verbose)
