d0f722c8e5bb47b443717614fada7bd8ff0968cc
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-05-13 13:31:45 +0100

    Fix and test microcircuit

--- p8_integration_tests/scripts_test/test_microcircuit.py
+++ p8_integration_tests/scripts_test/test_microcircuit.py
@@ -1,5 +1,7 @@
 from p8_integration_tests.scripts_test.script_checker import ScriptChecker
 import os
+import sys
+import stat
 
 
 class TestMicrocircuit(ScriptChecker):
@@ -15,8 +17,12 @@ class TestMicrocircuit(ScriptChecker):
         if not os.path.exists(microcircuit_dir):
             parent_dir = os.path.dirname(spynnaker8_dir)
             microcircuit_dir = os.path.join(parent_dir, "microcircuit_model")
+        # Get the microcircuit sub directory
+        microcircuit_dir = os.path.join(microcircuit_dir, "microcircuit")
+        sys.path.append(microcircuit_dir)
         microcircuit_script = os.path.join(
-            microcircuit_dir, "microcircuit", "microcircuit.py")
+            microcircuit_dir, "microcircuit.py")
+        os.makedirs("results")
         self.check_script(microcircuit_script, False)
         for result_file in [
                 "spikes_L23E.pkl", "spikes_L23I.pkl",
@@ -24,5 +30,6 @@ class TestMicrocircuit(ScriptChecker):
                 "spikes_L5E.pkl", "spikes_L5I.pkl",
                 "spikes_L6E.pkl", "spikes_L6I.pkl",
                 "spiking_activity.png"]:
-            assert(os.path.exists(os.path.join(
-                microcircuit_dir, "microcircuit", "results", result_file)))
+            result_path = os.path.join("results", result_file)
+            assert(os.path.exists(result_path))
+            assert(os.stat(result_path)[stat.ST_SIZE])
