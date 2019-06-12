aaefc0ba44ae15f37c8d325d9e327bf87d7056da
Nicky <Buttigieg>
2019-06-12 21:12:09 +0100

    Init dir_pref for each neuron

--- unittests/navigation_testing/single_pop_model/structure_grid_cell.py
+++ unittests/navigation_testing/single_pop_model/structure_grid_cell.py
@@ -3,22 +3,23 @@ import matplotlib.pyplot as plt
 from pyNN.utility.plotting import Figure, Panel
 from pyNN.space import Grid2D
 from pyNN.random import RandomDistribution, NumpyRNG
+import utilities as util
 
 '''
-Model Parameters
+Paper Model Parameters
 cell_rows = 132
 cell_cols = 112
 timestep = 1
-rest_pot = -65
-reset_pot = -67
-thres_pot = -63
-membrane_time_constant = 10
-membrane_resistance = 10
-refractory_period = 5
-input_curr = 2.4
-vel_curr = 0.175
+rest_pot = -65mV
+reset_pot = -67mV
+thres_pot = -63mV
+membrane_time_constant = 10ms
+membrane_resistance = 10 ohm
+refractory_period = 5ms
+input_curr = 2.4mA
+vel_curr = 0.175mA
 orientation_pref_shift = 2
-syn_weight = -0.6
+syn_weight = -0.6mV
 syn_delay = 5*rand()
 '''
 
@@ -29,19 +30,18 @@ grid_col = 2
 
 # Post-synapse population
 neuron_params = {
-    "v_thresh": -63,
-    "v_reset": -67,
+    "v_thresh": -50,
+    "v_reset": -65,
     "v_rest": -65,
-    "i_offset": 2.4,  # DC input
-    "i_vel": 0.175,
-    "tau_m": 10,  # membrane time constant
-    "tau_refrac": 5,
-    # "dir_pref": [1, 4, 3, 2]
+    "i_offset": 1,  # DC input
+    "i_vel_drive": 0,
+    "tau_m": 20,  # membrane time constant
+    "tau_refrac": 0.1,
 }
 
-pop_grid = Grid2D(aspect_ratio=1.0, dx=1.0, dy=1.0, x0=-grid_col / 2, y0=-grid_row / 2, z=0, fill_order='sequential')
+pop_grid = Grid2D(aspect_ratio=1.0, dx=1.0, dy=1.0, x0=0, y0=0, z=0, fill_order='sequential')
 rng = NumpyRNG(seed=41, parallel_safe=True)
-v_init = RandomDistribution('uniform', (-70, -60), rng)
+v_init = RandomDistribution('uniform', (-70, -55), rng)
 pop_exc = p.Population(grid_row * grid_col,
                        p.extra_models.GridCell(**neuron_params),
                        cellparams=None,
@@ -50,27 +50,12 @@ pop_exc = p.Population(grid_row * grid_col,
                        label="Grid cells"
                        )
 
-
-# Initialise neuron directional preference
-def init_dir_pref(pos):
-    x, y = pos.T
-    if x % 2 == 0 and y % 2 == 0:
-        return 1
-    elif x % 2 != 0 and y % 2 != 0:
-        return 4
-    elif x % 2 != 0 and y % 2 == 0:
-        return 3
-    elif x % 2 == 0 and y % 2 != 0:
-        return 2
-
-
-pop_exc.set(dir_pref=lambda i: init_dir_pref(pop_exc.position_generator(i)))
-
+pop_exc.set(dir_pref=lambda i: util.init_dir_pref(pop_exc.positions[i]))
+print(pop_exc.get('dir_pref'))
 pop_exc.record("all")
-# pop_exc.describe(template='grid_cell_pop_default.txt', engine='default')
+print(pop_exc.describe(template='population_default.txt', engine='default'))
 
 p.run(runtime)
-
 exc_data = pop_exc.get_data()
 
 # Plot
@@ -78,14 +63,16 @@ F = Figure(
     # plot data for postsynaptic neuron
     Panel(exc_data.segments[0].filter(name='v')[0],
           ylabel="Membrane potential (mV)",
-          data_labels=[pop_exc.label], yticks=True, xlim=(0, runtime)
+          xlabel="Time (ms)",
+          data_labels=[pop_exc.label], yticks=True, xticks=True, xlim=(0, runtime)
           ),
     Panel(exc_data.segments[0].filter(name='gsyn_exc')[0],
           ylabel="gsyn excitatory (mV)",
-          data_labels=[pop_exc.label], yticks=True, xlim=(0, runtime)
+          xlabel="Time (ms)",
+          data_labels=[pop_exc.label], yticks=True, xticks=True, xlim=(0, runtime)
           ),
     Panel(exc_data.segments[0].spiketrains,
-          yticks=True, markersize=2, xlim=(0, runtime)
+          yticks=True, xticks=True, markersize=2, xlim=(0, runtime)
           ),
 )
 
