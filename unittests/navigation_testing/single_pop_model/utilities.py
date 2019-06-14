c75300ce3759056c9f18c063fa0afecd0292bfa8
Nicky <Buttigieg>
2019-06-14 17:49:21 +0100

    Find directional preference of connection

--- unittests/navigation_testing/single_pop_model/utilities.py
+++ unittests/navigation_testing/single_pop_model/utilities.py
@@ -1,14 +1,11 @@
-from math import radians
-
-
 # Initialise neuron directional preference
-def init_dir_pref(pos):
+def get_dir_pref(pos):
     x, y, z = pos.T
     if x % 2 == 0 and y % 2 == 0:
-        return radians(0)  # N
+        return [0, 1]  # N
     elif x % 2 != 0 and y % 2 != 0:
-        return radians(180)  # S
+        return [0, -1]  # S
     elif x % 2 != 0 and y % 2 == 0:
-        return radians(270)  # W
+        return [-1, 0]  # W
     elif x % 2 == 0 and y % 2 != 0:
-        return radians(90)  # E
+        return [1, 0]  # E
