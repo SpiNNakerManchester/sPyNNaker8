6dcd5a81debc9a029711f52a5dffb4ba126a4970
alan-stokes <alan.barry.stokes@gmail.com>
2019-07-25 17:21:29 +0100

    CHIMMMMPPPP!!!!!

--- spynnaker8/__init__.py
+++ spynnaker8/__init__.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# chimp!!!!
+
 # common imports
 import numpy as __numpy
 from six import iteritems
