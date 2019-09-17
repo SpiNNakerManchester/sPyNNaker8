ae9b699c0191d92a0435faf37abea03677514099
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-09-17 09:16:34 +0100

    Improve testing again

--- p8_integration_tests/quick_test/test_external_devices/test_live_neuron_voltage.py
+++ p8_integration_tests/quick_test/test_external_devices/test_live_neuron_voltage.py
@@ -22,29 +22,34 @@ from spynnaker.pyNN.external_devices_models\
 from data_specification.enums.data_type import DataType
 import decimal
 import numpy
-
-DEVICE_KEY = 0xF
+from collections import defaultdict
 
 
 class Translator(AbstractEthernetTranslator):
 
-    def __init__(self):
-        self.voltages = list()
+    def __init__(self, devices):
+        self.__keys = {device.device_control_key for device in devices}
+        self.voltages = defaultdict(list)
 
     def translate_control_packet(self, multicast_packet):
-        if multicast_packet.key != DEVICE_KEY:
+        if multicast_packet.key not in self.__keys:
             raise Exception("Unknown key {} received".format(
                 multicast_packet.key))
         voltage = multicast_packet.payload
-        self.voltages.append(
+        self.voltages[multicast_packet.key].append(
             (float)(decimal.Decimal(voltage) / DataType.S1615.scale))
 
 
 class Device(AbstractMulticastControllableDevice):
 
+    def __init__(self, key, time_betweeen_sending, partition):
+        self.__key = key
+        self.__time_between_sending = time_betweeen_sending
+        self.__partition = partition
+
     @property
     def device_control_key(self):
-        return DEVICE_KEY
+        return self.__key
 
     @property
     def device_control_max_value(self):
@@ -56,7 +61,7 @@ class Device(AbstractMulticastControllableDevice):
 
     @property
     def device_control_partition_id(self):
-        return "DEVICE_CONTROL"
+        return self.__partition
 
     @property
     def device_control_scaling_factor(self):
@@ -68,7 +73,7 @@ class Device(AbstractMulticastControllableDevice):
 
     @property
     def device_control_timesteps_between_sending(self):
-        return 10
+        return self.__time_between_sending
 
     @property
     def device_control_uses_payload(self):
@@ -82,34 +87,52 @@ def spike_receiver(label, time, spikes):
 def live_neuron_voltage():
     p.setup(1.0)
     run_time = 1000.0
-    translator = Translator()
-    devices = [Device()]
     create_edges = False
+    time_1 = 10
+    key_1 = 0x1
+    devices_1 = [Device(key_1, time_1, "DEVICE_1")]
+    translator_1 = Translator(devices_1)
+    model_1 = p.external_devices.ExternalDeviceLifControl(
+        devices_1, create_edges, translator_1)
+    time_2_1 = 5
+    key_2_1 = 0xE
+    time_2_2 = 3
+    key_2_2 = 0xF
+    devices_2 = [Device(key_2_1, time_2_1, "DEVICE_1"),
+                 Device(key_2_2, time_2_2, "DEVICE_2")]
+    translator_2 = Translator(devices_2)
+    model_2 = p.external_devices.ExternalDeviceLifControl(
+        devices_2, create_edges, translator_2)
     conn = p.external_devices.SpynnakerLiveSpikesConnection(
         receive_labels=["stim"], local_port=None)
     conn.add_receive_callback("stim", spike_receiver)
     stim = p.Population(1, p.SpikeSourceArray(range(0, 1000, 100)),
                         label="stim")
-    model = p.external_devices.ExternalDeviceLifControl(
-        devices, create_edges, translator)
     p.external_devices.activate_live_output_for(
         stim, database_notify_port_num=conn.local_port)
-    ext_pop = p.external_devices.EthernetControlPopulation(1, model)
+    ext_pop = p.external_devices.EthernetControlPopulation(
+        len(devices_1), model_1)
     ext_pop.record(["v"])
+    ext_pop_2 = p.external_devices.EthernetControlPopulation(
+        len(devices_2), model_2)
+    ext_pop_2.record(["v"])
     p.Projection(
         stim, ext_pop, p.OneToOneConnector(), p.StaticSynapse(1.0, 1.0))
     p.run(run_time)
     v = ext_pop.get_data("v").segments[0].analogsignals[0].as_array()[:, 0]
     p.end()
-    assert(
-        len(translator.voltages) ==
-        run_time // devices[0].device_control_timesteps_between_sending)
-    relevant_v = v[
-        1:1000:devices[0].device_control_timesteps_between_sending]
+    relevant_v = v[1:1000:time_1]
     print(v)
     print(relevant_v)
-    print(translator.voltages)
-    assert(numpy.array_equal(relevant_v, translator.voltages))
+    print(len(translator_1.voltages[key_1]), translator_1.voltages[key_1])
+    print(len(translator_2.voltages[key_2_1]), translator_2.voltages[key_2_1])
+    print(len(translator_2.voltages[key_2_2]), translator_2.voltages[key_2_2])
+    assert(len(translator_1.voltages[key_1]) == run_time // time_1)
+    assert(len(translator_2.voltages[key_2_1]) == run_time // time_2_1)
+    assert(len(translator_2.voltages[key_2_2]) == run_time // time_2_2)
+    assert(numpy.array_equal(relevant_v, translator_1.voltages[key_1]))
+    assert(numpy.sum(translator_2.voltages[key_2_1]) == 0)
+    assert(numpy.sum(translator_2.voltages[key_2_2]) == 0)
 
 
 class TestLiveNeuronVoltage(BaseTestCase):
