a2702b537cef637b72bcd8e0e5687c37c32dc9ef
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2019-10-24 08:40:34 +0100

    moved integration test

--- spynnaker8/__init__.py
+++ spynnaker8/__init__.py
@@ -12,5 +12,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Please replace any import spynakker8 as .... with import spynaker as ...
--- unittests/__init__.py
+++ unittests/__init__.py
@@ -13,20 +13,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from p8_integration_tests.base_test_case import BaseTestCase
-import p8_integration_tests.scripts.synfire_npop_run as synfire_npop_run
-
-n_neurons = 10  # number of neurons in each population
-n_pops = 630
-
-
-class Synfire6300n10pop10pc48chipsNoDelaysSpikeRecording(BaseTestCase):
-
-    def test_run(self):
-        synfire_npop_run.do_run(n_neurons, n_pops=n_pops,
-                                neurons_per_core=n_neurons)
-
-
-if __name__ == '__main__':
-    x = Synfire6300n10pop10pc48chipsNoDelaysSpikeRecording()
-    x.test_run()
+# Please replace any import spynakker8 as .... with import spynaker as ...
