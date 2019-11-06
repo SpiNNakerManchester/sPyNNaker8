52680fc8aa0e84fea7c0b6c44278e3a26a5708ae
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-11-06 12:20:19 +0000

    fixed test

--- p8_integration_tests/quick_test/test_iobuf/test_only_cores_recorded/test_only_core_recording.py
+++ p8_integration_tests/quick_test/test_iobuf/test_only_cores_recorded/test_only_core_recording.py
@@ -24,64 +24,53 @@ class TestOnlyCoresRecording(BaseTestCase):
     def do_run(self):
         # From the config file
         requested_cores = [(0, 0, 1), (0, 0, 3), (1, 1, 1)]
-        sim.setup(timestep=1.0)
+        sim.setup(timestep=1.0, n_boards_required=1)
         sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 100)
 
         machine = globals_variables.get_simulator().machine
-        existing_chips = list()
-        while len(existing_chips) < 2:
-            for chip in machine.chips:
-                existing_chips.append(chip)
-        coords_first_chip = list()
-        while len(coords_first_chip) < 2:
-            chip = existing_chips[0]
-            for processor_id in chip.processors:
-                coords_first_chip.append([chip.x, chip.y, processor_id])
-        coords_second_chip = list()
-        while len(coords_first_chip) < 1:
-            chip = existing_chips[1]
-            for processor_id in chip.processors:
-                coords_second_chip.append([chip.x, chip.y, processor_id])
 
-        input = sim.Population(
-            1, sim.SpikeSourceArray(spike_times=[0]), label="input")
+        input1 = sim.Population(
+            1, sim.SpikeSourceArray(spike_times=[0]), label="input1")
         input2 = sim.Population(
-            1, sim.SpikeSourceArray(spike_times=[0]), label="input")
-        coord1 = coords_first_chip[0]
-        input.set_constraint(
-            ChipAndCoreConstraint(coord1[0], coord1[1], coord1[2]))
-        coord2 = coords_first_chip[1]
+            1, sim.SpikeSourceArray(spike_times=[0]), label="input2")
+        pop_1 = sim.Population(5, sim.IF_curr_exp(), label="pop_1")
+        pop_2 = sim.Population(5, sim.IF_curr_exp(), label="pop_2")
+        sim.Projection(input1, pop_1, sim.AllToAllConnector(),
+                       synapse_type=sim.StaticSynapse(weight=5, delay=1))
+        sim.Projection(input2, pop_2, sim.AllToAllConnector(),
+                       synapse_type=sim.StaticSynapse(weight=5, delay=1))
+
+        # Make sure there is stuff at the cores specified in the cfg file
+        input1.set_constraint(
+            ChipAndCoreConstraint(0, 0, 1))
         input2.set_constraint(
-            ChipAndCoreConstraint(coord2[0], coord2[1], coord2[2]))
-        pop_1 = sim.Population(100, sim.IF_curr_exp(), label="pop_1")
-        coord3 = coords_second_chip[0]
-        pop_1.set_constraint(
-            ChipAndCoreConstraint(coord3[0], coord3[1], coord3[2]))
-        sim.Projection(input, pop_1, sim.AllToAllConnector(),
-                       synapse_type=sim.StaticSynapse(weight=5, delay=18))
+            ChipAndCoreConstraint(0, 0, 3))
+        # While there must be a chip 0,0  chip 1,1 could be missing
+        if machine.is_chip_at(1, 1):
+            pop_1.set_constraint(
+                ChipAndCoreConstraint(1, 1, 1))
+        # Make sure there is stuff at a core not specified in the cfg file
+        pop_2.set_constraint(
+            ChipAndCoreConstraint(0, 0, 2))
+
         sim.run(500)
 
         provenance_files = self.get_app_iobuf_files()
         sim.end()
 
-        # extract_iobuf_from_cores = 0,0,1
         self.assertIn(
-            "iobuf_for_chip_{}_{}_processor_id_{}.txt".format(
-                coord1[0], coord1[1], coord1[2]), provenance_files)
+            "iobuf_for_chip_0_0_processor_id_1.txt", provenance_files)
+        self.assertNotIn(
+            "iobuf_for_chip_0_0_processor_id_2.txt", provenance_files)
         self.assertIn(
-            "iobuf_for_chip_{}_{}_processor_id_{}.txt".format(
-                coord2[0], coord2[1], coord2[2]), provenance_files)
-        self.assertIn(
-            "iobuf_for_chip_{}_{}_processor_id_{}.txt".format(
-                coord3[0], coord3[1], coord3[2]), provenance_files)
+            "iobuf_for_chip_0_0_processor_id_3.txt", provenance_files)
         self.assertNotIn(
             "iobuf_for_chip_0_0_processor_id_4.txt", provenance_files)
+        if machine.is_chip_at(1, 1):
+            self.assertIn(
+                "iobuf_for_chip_1_1_processor_id_1.txt", provenance_files)
         self.assertNotIn(
-            "iobuf_for_chip_0_0_processor_id_5.txt", provenance_files)
-        self.assertNotIn(
-            "iobuf_for_chip_0_0_processor_id_6.txt", provenance_files)
-        self.assertIn(
-            "iobuf_for_chip_1_1_processor_id_1.txt", provenance_files)
+            "iobuf_for_chip_1_1_processor_id_2.txt", provenance_files)
 
     def test_do_run(self):
         self.runsafe(self.do_run)
