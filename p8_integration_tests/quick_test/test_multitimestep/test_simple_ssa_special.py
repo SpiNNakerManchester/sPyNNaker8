4749e7d692b96d5c3e2ec6adefa3d5c4224f1e40
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-01-02 17:55:55 +0000

    work in progress

--- p8_integration_tests/quick_test/test_multitimestep/test_simple_ssa_special.py
+++ p8_integration_tests/quick_test/test_multitimestep/test_simple_ssa_special.py
@@ -13,42 +13,110 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import division
+import math
+from spinn_utilities.helpful_functions import lcm
 import spynnaker8 as sim
 from p8_integration_tests.base_test_case import BaseTestCase
 
 class TestSimpleSsaSpecial(BaseTestCase):
 
-    def do_run(self):
-        sim.setup(timestep=1.0)
-        sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 100)
+    def do_script(self, sys_timestep, ssa_timestep, ssa_spike_time,
+                  if_curr_timestep, delay, runtime):
+        sim.setup(timestep=sys_timestep)
 
-        pop_1 = sim.Population(1, sim.IF_curr_exp(), label="pop_1")
-        input = sim.Population(1, sim.SpikeSourceArray(), label="input")
-        # Hack in a different timestep
-        input._vertex._timestep_in_us = 3000
-        # Spiketimes must be provided after hacked timestep
-        input._vertex.spike_times = [5]
+        input = sim.Population(
+            1, sim.SpikeSourceArray([ssa_spike_time]), label="input",
+            additional_parameters={"timestep_in_us": ssa_timestep})
         input.record("spikes")
-        sim.Projection(input, pop_1, sim.OneToOneConnector(),
-                       synapse_type=sim.StaticSynapse(weight=5, delay=1))
+        pop_1 = sim.Population(
+            1, sim.IF_curr_exp(), label="pop_1",
+            additional_parameters={"timestep_in_us": if_curr_timestep})
         pop_1.record(["spikes", "v"])
-        simtime = 20
-        sim.run(simtime)
 
-        neo = input.get_data(variables=["spikes"])
-        i_spikes = neo.segments[0].spiketrains
-        self.assertEquals(len(i_spikes), 1)
-        # 5 is rounded up to 6
-        self.assertEquals(i_spikes[0].magnitude, 6)
-        neo = pop_1.get_data(variables=["spikes", "v"])
-        spikes = neo.segments[0].spiketrains
-        # Spike sent timestgep after the 6 so 9. Pop spikes 6 steps later so 15
-        self.assertEquals(spikes[0].magnitude, 15)
-        v = neo.segments[0].filter(name='v')[0]
-        # Runtime 20ms rounded up to next lcm timestep of 3000us so 21ms
-        self.assertEquals(v.size, 21)
-        print("Ran for {} timesteps".format(v.size))
+        sim.Projection(input, pop_1, sim.OneToOneConnector(),
+                       synapse_type=sim.StaticSynapse(weight=5, delay=delay))
+        sim.run(runtime)
+        i_neo = input.get_data(variables=["spikes"])
+        p_neo = pop_1.get_data(variables=["spikes", "v"])
         sim.end()
 
-    def test_do_run(self):
-        self.runsafe(self.do_run)
+        # If specific was None the sys should have been used
+        if ssa_timestep is None:
+            ssa_timestep =  int(sys_timestep * 1000)
+        if if_curr_timestep is None:
+            if_curr_timestep = int(sys_timestep * 1000)
+
+        # Calc when input sent spike
+        input_spike_in_us = ssa_spike_time * 1000
+        rounded_input_in_us =  ssa_timestep * int(
+            math.ceil(input_spike_in_us/ssa_timestep))
+        rounded_input_spike_in_ms = rounded_input_in_us / 1000
+
+        i_spikes = i_neo.segments[0].spiketrains
+        self.assertEquals(len(i_spikes), 1)
+        self.assertEquals(i_spikes[0].magnitude, rounded_input_spike_in_ms)
+
+        # Calc when the input spike received
+        # Arrives with a minor delay  (10 is a quess)
+        estimate_arrival_in_us = rounded_input_in_us + 10
+        # Arrive At the start of an input timestep (ceil to get to next one)
+        estimate_arrival_in_step = math.ceil(
+            estimate_arrival_in_us / if_curr_timestep)
+        # Assumed to have actually been sent in the previous timestep
+        calc_arrival_in_steps  = estimate_arrival_in_step - 1
+        calc_arrival_in_us = calc_arrival_in_steps * if_curr_timestep
+
+        #round delay up
+        delay_timesteps = int(math.ceil((delay * 1000) / if_curr_timestep))
+        delay_in_us = delay_timesteps * if_curr_timestep
+
+        # 6000 is what I think the timke to spike is
+        calc_spike_in_us = calc_arrival_in_us + delay_in_us + 6000
+        calc_spike_in_timestep = int(
+            math.ceil(calc_spike_in_us / if_curr_timestep))
+
+        lcm_timestep = lcm(int(sys_timestep * 1000), ssa_timestep, if_curr_timestep)
+        runtime_in_lcm = math.ceil(runtime * 1000 / lcm_timestep)
+        runtime_in_us = runtime_in_lcm * lcm_timestep
+        runtime_in_pop_timesteps = runtime_in_us / if_curr_timestep
+
+        spikes = p_neo.segments[0].spiketrains
+        # Spike sent timestgep after the 6 so 9. Pop spikes 6 steps later so 15
+        self.assertEquals(spikes[0].magnitude, calc_spike_in_timestep)
+        v = p_neo.segments[0].filter(name='v')[0]
+        # Runtime 20ms rounded up to next lcm timestep of 3000us so 21ms
+        self.assertEquals(v.size, runtime_in_pop_timesteps)
+
+
+    def do_ssa_3000_5(self):
+        self.do_script(
+            sys_timestep=1.0, ssa_timestep=3000, ssa_spike_time=5,
+            if_curr_timestep=1000, delay=1, runtime=20)
+
+    def test_ssa_3000_5(self):
+        self.runsafe(self.do_ssa_3000_5)
+
+    def do_ssa_3000_3(self):
+        self.do_script(
+            sys_timestep=1.0, ssa_timestep=3000, ssa_spike_time=3,
+            if_curr_timestep=1000, delay=1, runtime=20)
+
+    def test_ssa_3000_3(self):
+        self.runsafe(self.do_ssa_3000_5)
+
+    def do_ssa_1000_5(self):
+        self.do_script(
+            sys_timestep=1.0, ssa_timestep=1000, ssa_spike_time=5,
+            if_curr_timestep=1000, delay=1, runtime=20)
+
+    def test_ssa_1000_5(self):
+        self.runsafe(self.do_ssa_1000_5)
+
+    def do_simple(self):
+        self.do_script(
+            sys_timestep=1.0, ssa_timestep=1000, ssa_spike_time=0,
+            if_curr_timestep=1000, delay=1, runtime=20)
+
+    def test_simple(self):
+        self.runsafe(self.do_simple)
