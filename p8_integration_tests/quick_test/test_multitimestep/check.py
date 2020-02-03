db1afceefdada150ff7f469443dceca5ac390d3f
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2020-02-03 17:19:24 +0000

    work in progress

--- p8_integration_tests/quick_test/test_multitimestep/check.py
+++ p8_integration_tests/quick_test/test_multitimestep/check.py
@@ -1,6 +1,8 @@
 import math
 import spynnaker8 as sim
-from p8_integration_tests.quick_test.test_multitimestep.multi_if_curr_exp_base import MultiIFCurrExpBase
+from p8_integration_tests.quick_test.test_multitimestep.\
+    multi_if_curr_exp_base import MultiIFCurrExpBase
+
 
 def get_expected_spike_arrival(ssa_spike_time, ssa_timestep):
     # Calc when input sent spike
@@ -13,10 +15,13 @@ def get_expected_spike_arrival(ssa_spike_time, ssa_timestep):
     return rounded_input_in_us + ssa_timestep
 
 
-def get_expected_spike(estimate_arrival_in_us, delay, if_curr_timestep):
+def get_expected_spike(index, estimate_arrival_in_us, delay, if_curr_timestep):
     # Arrive during an input timestep
     estimate_arrival_in_step = math.floor(
         estimate_arrival_in_us / if_curr_timestep)
+    # this is to cover the time skuew
+    if index < 2:
+        estimate_arrival_in_step -= 1
     # Assumed to have actually been sent in the previous timestep
     # This could even be -1 if the ssa timestep is smaller than pop's
     calc_arrival_in_steps = estimate_arrival_in_step - 1
@@ -33,19 +38,18 @@ def get_expected_spike(estimate_arrival_in_us, delay, if_curr_timestep):
     # round up to next timestep
     calc_spike_rounded = if_curr_timestep * int(
         math.ceil(calc_spike_in_us / if_curr_timestep))
+    print(if_curr_timestep, calc_arrival_in_steps, calc_arrival_in_us, delay_in_us , calc_spike_in_us, calc_spike_rounded)
     return calc_spike_rounded / 1000
 
-    print(rounded_input_spike_in_ms, calc_arrival_in_us, delay_in_us,
-          calc_spike_in_ms, runtime_in_pop_timesteps)
 
 def lcm(sys_timestep, if_curr_timestep ):
     lcm_timestep = lcm(int(sys_timestep * 1000),
                        ssa_timestep, if_curr_timestep)
 
 ssa_spike_time = 4
-ssa_timestep = 1000 # 2200
+ssa_timestep = 2200
 delay = 6
-sim.setup(timestep=1)
+sim.setup(timestep=2)
 sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 3)
 input = sim.Population(
     1, sim.SpikeSourceArray([ssa_spike_time]), label="input",
@@ -60,16 +64,6 @@ proj = sim.Projection(input, pop_1, sim.AllToAllConnector(),
                       synapse_type=sim.StaticSynapse(weight=5, delay=delay))
 sim.run(33)
 
-estimate_arrival_in_us = get_expected_spike_arrival(
-    ssa_spike_time, ssa_timestep)
-
-if_curr_timesteps = pop_1._vertex.timesteps_in_us
-calc_spike_in_ms = []
-for i in range(len(pop_1._vertex.timesteps_in_us)):
-    calc_spike_in_ms.append(get_expected_spike(
-        estimate_arrival_in_us, delay, if_curr_timesteps[i]))
-
-print(calc_spike_in_ms)
 proj.get("delay", "list")
 i_neo = input.get_data(variables=["spikes"])
 i_spikes = i_neo.segments[0].spiketrains
@@ -81,6 +75,19 @@ print(spikes)
 v = neo.segments[0].filter(name='v')[0]
 print(v)
 sim.end()
+
+estimate_arrival_in_us = get_expected_spike_arrival(
+    ssa_spike_time, ssa_timestep)
+print(estimate_arrival_in_us)
+if_curr_timesteps = pop_1._vertex.timesteps_in_us
+calc_spike_in_ms = []
+for i in range(len(pop_1._vertex.timesteps_in_us)):
+    calc_spike_in_ms.append(get_expected_spike(
+        i, estimate_arrival_in_us, delay, if_curr_timesteps[i]))
+
+print(calc_spike_in_ms)
+
+
 
 """
 1000 = 17
