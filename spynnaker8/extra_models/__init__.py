73776666104c84ebed23443fbb934b2f9e55ad3b
such-a-git <adam.perrett@manchester.ac.uk>
2020-06-02 15:31:16 +0100

    change to init to include left-right task

--- spynnaker8/extra_models/__init__.py
+++ spynnaker8/extra_models/__init__.py
@@ -30,7 +30,7 @@ from spynnaker.pyNN.models.neuron.builds import (
     IFCurDelta, IFCurrExpCa2Adaptive, IFCurrDualExpBase as
     IF_curr_dual_exp, IzkCondExpBase as
     Izhikevich_cond, IFCurrExpSEMDBase as
-    IF_curr_exp_sEMD, EPropAdaptive, StoreRecallReadout, SinusoidReadout)
+    IF_curr_exp_sEMD, EPropAdaptive, StoreRecallReadout, SinusoidReadout, LeftRightReadout)
 
 
 from spynnaker.pyNN.models.neuron.plasticity.stdp.timing_dependence import TimingDependenceEprop
@@ -40,7 +40,7 @@ __all__ = [
     # sPyNNaker 8 models
     'IFCurDelta', 'IFCurrExpCa2Adaptive', 'IFCondExpStoc',
     'Izhikevich_cond', 'IF_curr_dual_exp', 'IF_curr_exp_sEMD',
-    'EPropAdaptive', "StoreRecallReadout", "SinusoidReadout",
+    'EPropAdaptive', "StoreRecallReadout", "SinusoidReadout", "LeftRightReadout",
 
     # sPyNNaker 8 plastic stuff
     'WeightDependenceAdditiveTriplet',
