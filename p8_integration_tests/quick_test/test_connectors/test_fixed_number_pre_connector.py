e780ecdfe072d39ed29c1f1f9d02ec1d09b6c371
alan-stokes <alan.barry.stokes@gmail.com>
2020-10-22 15:33:01 +0100

    reengage the test

--- p8_integration_tests/quick_test/test_connectors/test_fixed_number_pre_connector.py
+++ p8_integration_tests/quick_test/test_connectors/test_fixed_number_pre_connector.py
@@ -12,8 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from unittest import SkipTest
-
 import spynnaker8 as sim
 from p8_integration_tests.base_test_case import BaseTestCase
 
@@ -69,28 +67,24 @@ class TestFixedNumberPreConnector(BaseTestCase):
         sim.end()
 
     def test_with_delays(self):
-        try:
-            sim.setup(1.0)
-            # Break up the pre population as that is where delays happen
-            sim.set_number_of_neurons_per_core(sim.SpikeSourceArray, 50)
-            pop1 = sim.Population(100, sim.SpikeSourceArray([1]), label="pop1")
-            pop2 = sim.Population(10, sim.IF_curr_exp(), label="pop2")
-            pop2.record("spikes")
-            # Choose to use delay extensions
-            synapse_type = sim.StaticSynapse(weight=0.5, delay=17)
-            conn = sim.FixedNumberPreConnector(10)
-            projection = sim.Projection(
-                pop1, pop2, conn, synapse_type=synapse_type)
-            delays = projection.get(["delay"], "list")
-            sim.run(30)
-            # There are 100 connections, as there are 10 for each post-neuron
-            assert(len(delays) == 100)
-            # If the delays are done right, all pre-spikes should arrive at the
-            # same time causing each neuron in the post-population to spike
-            spikes = pop2.get_data("spikes").segments[0].spiketrains
-            for s in spikes:
-                assert(len(s) == 1)
-            sim.end()
-        except Exception:
-            raise SkipTest(
-                "https://github.com/SpiNNakerManchester/sPyNNaker/issues/902")
+        sim.setup(1.0)
+        # Break up the pre population as that is where delays happen
+        sim.set_number_of_neurons_per_core(sim.SpikeSourceArray, 50)
+        pop1 = sim.Population(100, sim.SpikeSourceArray([1]), label="pop1")
+        pop2 = sim.Population(10, sim.IF_curr_exp(), label="pop2")
+        pop2.record("spikes")
+        # Choose to use delay extensions
+        synapse_type = sim.StaticSynapse(weight=0.5, delay=17)
+        conn = sim.FixedNumberPreConnector(10)
+        projection = sim.Projection(
+            pop1, pop2, conn, synapse_type=synapse_type)
+        delays = projection.get(["delay"], "list")
+        sim.run(30)
+        # There are 100 connections, as there are 10 for each post-neuron
+        assert(len(delays) == 100)
+        # If the delays are done right, all pre-spikes should arrive at the
+        # same time causing each neuron in the post-population to spike
+        spikes = pop2.get_data("spikes").segments[0].spiketrains
+        for s in spikes:
+            assert(len(s) == 1)
+        sim.end()
