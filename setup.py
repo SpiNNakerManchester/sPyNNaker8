72ce0af98fe54c1b45276617b60993e2343f65b6
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2021-01-26 15:15:12 +0000

    Turns out we usually need setuptools to do our installs

--- setup.py
+++ setup.py
@@ -14,8 +14,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-from setuptools import setup
 from collections import defaultdict
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 __version__ = None
 __version_type__ = None
